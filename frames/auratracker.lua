3d77543c3e6dcef0974fee436044411b901bfeae
Valeriy Voronov <i.lightspark@gmail.com>
2014-06-17 02:31:16 +0800

    ADDED: - support for SavedVariables per character, now we can save data between sessions! yay! TO-DO: - make tracker frame lockable - redraw NormalTextures for buttons... dun look good when scaled up.. - code cleanup

--- frames/auratracker.lua
+++ frames/auratracker.lua
@@ -1,10 +1,15 @@
 local _, ns = ...
 local C, M = ns.C, ns.M
 
-local buffList = {
+local LOCAL_CONFIG
+
+local DEFAULT_CONFIG = {
+	buffList = {
 	-- 116257,
 	-- 61316,
 	-- 7302,
+	},
+	trackerPoint = {"CENTER", UIParent, "CENTER", 0, 0},
 }
 
 local BUTTON_LAYOUT = {
@@ -18,9 +23,13 @@ local BUTTON_LAYOUT = {
 
 local auraTracker = CreateFrame("Frame", "oUF_LSAuraTackerBar", UIParent, "SecureHandlerStateTemplate")
 auraTracker:SetSize(264, 64)
-auraTracker:SetPoint("CENTER", 0, 0)
+auraTracker:SetMovable(1)
+auraTracker:EnableMouse(true)
+auraTracker:RegisterForDrag("LeftButton")
 auraTracker:RegisterUnitEvent("UNIT_AURA", "player", "vehicle")
 auraTracker:RegisterEvent("PLAYER_LOGIN")
+auraTracker:RegisterEvent("ADDON_LOADED")
+auraTracker:RegisterEvent("PLAYER_LOGOUT")
 
 auraTracker.buffs = {}
 auraTracker.buttons = {}
@@ -104,52 +113,106 @@ local function oUF_LSAuraButton_OnUpdate(self, elapsed)
 end
 
 local function oUF_LSAuraButton_OnEvent(...)
-	auraTracker.buffs = {}
-	for i = 1, 32 do
-		local name, _, iconTexture, count, buffType, duration, expirationTime, casterID, _, _, spellId = UnitBuff("player", i)
-		if name and tContains(buffList, spellId) then
-			local aura = {}
-			aura.id = spellId
-			aura.index = i
-			aura.icon = iconTexture
-			aura.expire = expirationTime
-			auraTracker.buffs[#auraTracker.buffs + 1] = aura
+	local _, event, arg3 = ...
+	if event == "UNIT_AURA" or event == "PLAYER_LOGIN" or event == "CUSTOM_FORCE_UPDATE" then
+		if event == "PLAYER_LOGIN" then
+			auraTracker:SetPoint(unpack(LOCAL_CONFIG.trackerPoint))
+			oUF_LSAuraTacker_ButtonSpawn(#LOCAL_CONFIG.buffList)
 		end
-	end
-	for i = #auraTracker.buffs + 1, 5 do
-		if auraTracker.buttons[i] then
-			auraTracker.buttons[i]:Hide()
-			auraTracker.buttons[i]:SetScript("OnUpdate", nil)
+		auraTracker.buffs = {}
+		for i = 1, 32 do
+			local name, _, iconTexture, count, buffType, duration, expirationTime, casterID, _, _, spellId = UnitBuff("player", i)
+			if name and tContains(LOCAL_CONFIG.buffList, spellId) then
+				local aura = {}
+				aura.id = spellId
+				aura.index = i
+				aura.icon = iconTexture
+				aura.expire = expirationTime
+				auraTracker.buffs[#auraTracker.buffs + 1] = aura
+			end
 		end
-	end
-	for i = 1, #auraTracker.buffs do
-		auraTracker.buttons[i]:Show()
-		auraTracker.buttons[i]:SetID(auraTracker.buffs[i].index)
-		auraTracker.buttons[i].id = i
-		auraTracker.buttons[i].icon:SetTexture(auraTracker.buffs[i].icon)
-		auraTracker.buttons[i]:SetScript("OnUpdate", oUF_LSAuraButton_OnUpdate)
+		for i = #auraTracker.buffs + 1, 5 do
+			if auraTracker.buttons[i] then
+				auraTracker.buttons[i]:Hide()
+				auraTracker.buttons[i]:SetScript("OnUpdate", nil)
+			end
+		end
+		for i = 1, #auraTracker.buffs do
+			auraTracker.buttons[i]:Show()
+			auraTracker.buttons[i]:SetID(auraTracker.buffs[i].index)
+			auraTracker.buttons[i].id = i
+			auraTracker.buttons[i].icon:SetTexture(auraTracker.buffs[i].icon)
+			auraTracker.buttons[i]:SetScript("OnUpdate", oUF_LSAuraButton_OnUpdate)
+		end
+	elseif event == "ADDON_LOADED" then
+		if arg3 ~= "oUF_LS" then return end
+	
+		local function initDB(db, defaults)
+			if type(db) ~= "table" then db = {} end
+			if type(defaults) ~= "table" then return db end
+			for k, v in pairs(defaults) do
+				if type(v) == "table" then
+					db[k] = initDB(db[k], v)
+				elseif type(v) ~= type(db[k]) then
+					db[k] = v
+				end
+			end
+			return db
+		end
+
+		oUF_LS_AURA_CONFIG = initDB(oUF_LS_AURA_CONFIG, DEFAULT_CONFIG)
+		LOCAL_CONFIG = oUF_LS_AURA_CONFIG
+
+	elseif event == "PLAYER_LOGOUT" then
+		local function cleanDB(db, defaults)
+			if type(db) ~= "table" then return {} end
+			if type(defaults) ~= "table" then return db end
+			for k, v in pairs(db) do
+				if type(v) == "table" then
+					if not next(cleanDB(v, defaults[k])) then
+						db[k] = nil
+					end
+				elseif v == defaults[k] then
+					db[k] = nil
+				end
+			end
+			return db
+		end
+
+		oUF_LS_AURA_CONFIG = cleanDB(oUF_LS_AURA_CONFIG, DEFAULT_CONFIG)
 	end
 end
 
 auraTracker:SetScript("OnEvent", oUF_LSAuraButton_OnEvent)
 
+
+auraTracker:SetScript("OnDragStart", function(self) 
+	self:StartMoving()
+end)
+
+auraTracker:SetScript("OnDragStop", function(self) 
+	self:StopMovingOrSizing()
+	local point, _, relativePoint, xOfs, yOfs = self:GetPoint()
+	LOCAL_CONFIG.trackerPoint = {point, "UIParent", relativePoint, xOfs, yOfs}
+end)
+
 ns.DebugTexture(auraTracker)
 
 SLASH_ATADD1 = '/atadd'
 local function AuraTracker_Add(msg)
-	table.insert(buffList, tonumber(msg))
-	oUF_LSAuraTacker_ButtonSpawn(#buffList)
-	oUF_LSAuraButton_OnEvent()
+	table.insert(LOCAL_CONFIG.buffList, tonumber(msg))
+	oUF_LSAuraTacker_ButtonSpawn(#LOCAL_CONFIG.buffList)
+	oUF_LSAuraButton_OnEvent(nil, "CUSTOM_FORCE_UPDATE")
 end
 SlashCmdList["ATADD"] = AuraTracker_Add
 
 SLASH_ATREM1 = '/atrem'
 local function AuraTracker_Remove(msg)
-	for k,v in pairs(buffList) do
+	for k,v in pairs(LOCAL_CONFIG.buffList) do
 		if v == tonumber(msg) then
-			table.remove(buffList, k)
+			table.remove(LOCAL_CONFIG.buffList, k)
 		end
 	end
-	oUF_LSAuraButton_OnEvent()
+	oUF_LSAuraButton_OnEvent(nil, "CUSTOM_FORCE_UPDATE")
 end
 SlashCmdList["ATREM"] = AuraTracker_Remove
