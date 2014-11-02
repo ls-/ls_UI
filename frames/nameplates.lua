1aba6a30328fc6f1307db96e5c21e5ad63e0d949
Valeriy Voronov <i.lightspark@gmail.com>
2014-11-03 00:48:00 +0800

    NEVER USE NAMEPLATE AS A PARENT! UIParent for president! That's the worst idea I had yesterday, rewrote nameplates, without weird scaling logic, why did I even bother with it?

--- frames/nameplates.lua
+++ frames/nameplates.lua
@@ -1,4 +1,5 @@
 local _, ns = ...
+local format, match = string.format, string.match
 
 local function RGBToHEX(r, g, b)
 	if type(r) == "table" then
@@ -8,11 +9,11 @@ local function RGBToHEX(r, g, b)
 			r, g, b = unpack(r)
 		end
 	end
-	return string.format("%02x%02x%02x", r * 255, g * 255, b * 255)
+	return format("%02x%02x%02x", r * 255, g * 255, b * 255)
 end
 
 local function lsNamePlate_GetColor(r, g, b, a)
-	r, g, b, a = string.format("%.2f", r), string.format("%.2f", g), string.format("%.2f", b), string.format("%.2f", a)
+	r, g, b, a = format("%.2f", r), format("%.2f", g), format("%.2f", b), format("%.2f", a)
 
 	if g + b == 0 then
 		return 0.9, 0.15, 0.15, 1
@@ -28,69 +29,54 @@ local function lsNamePlate_GetColor(r, g, b, a)
 end
 
 local function lsNamePlate_OnShow(self)
-	local healthbar, scale = self.health.bar, self.overlay.scale
-	local w, h = self:GetWidth(), self:GetHeight()
+	self.overlay:SetScale(1)
 
-	self.overlay:SetSize(math.floor(w * scale), math.ceil(h * scale))
-	healthbar:ClearAllPoints()
-	healthbar:SetPoint("TOP", self.overlay, "TOP", 0, -math.ceil(h / 15 * scale))
-	healthbar:SetPoint("LEFT", self.overlay, 0, 0)
-	healthbar:SetPoint("RIGHT", self.overlay, 0, 0)
-	healthbar:SetHeight(math.ceil(h / 3 * scale))
+	local sw, ow = tonumber(format("%d", self:GetWidth())), tonumber(format("%d", self.overlay:GetWidth()))
+	if sw < ow then
+		self.overlay:SetScale(0.65)
+	end
+
+	local healthbar = self.health.bar
 	healthbar:SetMinMaxValues(self.health:GetMinMaxValues())
 	healthbar:SetStatusBarColor(lsNamePlate_GetColor(self.health:GetStatusBarColor()))
 
-	-- healthbar.fg:SetPoint("TOPLEFT", -math.ceil(h / 10 * scale), math.ceil(h / 10 * scale))
-	-- healthbar.fg:SetPoint("BOTTOMRIGHT", math.ceil(h / 10 * scale), -math.ceil(h / 10 * scale))
-	healthbar.fg:SetSize(math.floor(w * 1.06 * scale), math.ceil(h * 0.6 * scale))
-	print(healthbar.fg:GetSize())
-	self.raid:ClearAllPoints()
-	self.raid:SetSize(math.floor(h * 0.95 * scale), math.floor(h * 0.95 * scale))
-	self.raid:SetPoint("LEFT", self.overlay, "RIGHT", math.ceil(w / 50 * scale), 0)
-
-	self.name:SetPoint("LEFT", self.overlay, -math.ceil(w * 0.045 * scale), 0)
-	self.name:SetPoint("RIGHT", self.overlay, math.ceil(w * 0.045 * scale), 0)
-
 	local name = self.name:GetText() or UNKNOWNOBJECT
-	local level = tonumber(self.level:GetText()) or -1
+	--[[ it's pretty weird, but cuz of nameplate re-usage,
+		sometimes we can have both level number and boss icon present ]]
+	local level = self.boss:IsShown() and -1 or tonumber(self.level:GetText())
 	local color = RGBToHEX(GetQuestDifficultyColor((level > 0) and level or 99))
 
 	if self.boss:IsShown() then
 		level = "??"
 	end
 
-	if self.dragon:IsShown() then
+	if self.elite:IsShown() then
 		level = level.."+"
 	end
 
 	self.name:SetFormattedText("|cff%s%s|r %s", color, level, name)
+
+	self.threat:ClearAllPoints()
+	self.threat:SetPoint("TOPLEFT", healthbar.fg, "TOPLEFT", -1, 1)
+	self.threat:SetPoint("BOTTOMRIGHT", healthbar.fg, "BOTTOMRIGHT", 1, 7)
+
+	self.overlay:Show()
+end
+
+local function lsNamePlate_OnHide(self)
+	self.overlay:Hide()
 end
 
 local function lsNamePlateCastBar_OnShow(self)
-	local castbar, parent, scale = self.bar, self.parent, self.parent.overlay.scale
-	local w, h = parent:GetWidth(), parent:GetHeight()
-
-	castbar:ClearAllPoints()
-	castbar:SetPoint("BOTTOM", parent.overlay, "BOTTOM", 0, math.ceil(h / 15 * scale))
-	castbar:SetPoint("LEFT", parent.overlay, 0, 0)
-	castbar:SetPoint("RIGHT", parent.overlay, 0, 0)
-	castbar:SetHeight(math.ceil(h / 3 * scale))
+	local castbar = self.bar
+	castbar:Show()
 	castbar:SetMinMaxValues(self:GetMinMaxValues())
 
-	castbar.fg:SetPoint("TOPLEFT", -math.ceil(h / 10 * scale), math.ceil(h / 10 * scale))
-	castbar.fg:SetPoint("BOTTOMRIGHT", math.ceil(h / 10 * scale), -math.ceil(h / 10 * scale))
-
 	castbar.icon:SetTexture(self.icon:GetTexture())
-	castbar.icon:SetSize(math.ceil(h * 0.95 * scale), math.ceil(h * 0.95 * scale))
-	castbar.icon:SetPoint("RIGHT", parent.overlay, "LEFT", -math.ceil(w / 50 * scale), 0)
+end
 
-	castbar.iconborder:SetPoint("TOPLEFT", castbar.icon, "TOPLEFT", -math.ceil(h / 15 * scale), math.ceil(h / 15 * scale))
-	castbar.iconborder:SetPoint("BOTTOMRIGHT", castbar.icon, "BOTTOMRIGHT", math.ceil(h / 15 * scale), -math.ceil(h / 15 * scale))
-
-	self.name:ClearAllPoints()
-	self.name:SetPoint("LEFT", castbar, 2, 0)
-	self.name:SetPoint("RIGHT", castbar, -2, 0)
-	self.name:SetJustifyH("LEFT")
+local function lsNamePlateCastBar_OnHide(self)
+	self.bar:Hide()
 end
 
 local function lsSetNamePlateStyle(self)
@@ -100,38 +86,48 @@ local function lsSetNamePlateStyle(self)
 	self.health, self.cast = self.barFrame:GetChildren()
 
 	self.name = self.nameFrame:GetRegions()
-	self.threat, self.border, _, self.level, self.boss, self.raid, self.dragon = self.barFrame:GetRegions()
-	_, self.cast.border, self.cast.shield, self.cast.icon, self.cast.name, self.cast.nameShadow = self.cast:GetRegions()
+	self.threat, self.border, _, self.level, self.boss, self.raid, self.elite = self.barFrame:GetRegions()
+	_, self.castborder, self.cast.shield, self.cast.icon, self.castname, self.castnameShadow = self.cast:GetRegions()
 
 	self.level:SetParent(ns.hiddenParentFrame)
-	self.dragon:SetParent(ns.hiddenParentFrame)
+	self.elite:SetParent(ns.hiddenParentFrame)
 	self.boss:SetTexture(nil)
 	self.border:SetTexture(nil)
 
 	self.cast.icon:SetParent(ns.hiddenParentFrame)
-	self.cast.nameShadow:SetTexture(nil)
-	self.cast.border:SetTexture(nil)
+	self.castnameShadow:SetTexture(nil)
+	self.castborder:SetTexture(nil)
 	self.cast.shield:SetTexture(nil)
 
-	self.overlay = CreateFrame("Frame", self:GetName().."Overlay", self)
-	self.overlay.scale = 0.77
-	self.overlay:SetPoint("TOP", self, "CENTER", 0, -2)
+	local overlay = CreateFrame("Frame", "ls"..self:GetName().."Overlay", UIParent)
+	overlay:SetSize(120, 32)
+	overlay:SetPoint("TOP", self, "CENTER", 0, -6)
+	overlay:Hide()
 
-	-- health
+	self.overlay = overlay
+
+	-- Health
 	self.health:GetStatusBarTexture():SetTexture(nil)
 
-	self.health.bar = CreateFrame("Statusbar", nil, self)
-	self.health.bar:SetStatusBarTexture(ns.M.textures.statusbar)
-	self.health.bar:SetStatusBarColor(0.15, 0.15, 0.15)
+	local healthBar = CreateFrame("Statusbar", nil, overlay)
+	healthBar:SetPoint("TOP", overlay, "TOP", 0, 0)
+	healthBar:SetPoint("LEFT", overlay, 0, 0)
+	healthBar:SetPoint("RIGHT", overlay, 0, 0)
+	healthBar:SetHeight(12)
+	healthBar:SetStatusBarTexture(ns.M.textures.statusbar)
+	healthBar:SetStatusBarColor(0.15, 0.15, 0.15)
 
-	self.health.bar.bg = self.health.bar:CreateTexture(nil, "BACKGROUND", nil, 0)
-	self.health.bar.bg:SetAllPoints(self.health.bar)
-	self.health.bar.bg:SetTexture(0.15, 0.15, 0.15)
+	healthBar.bg = healthBar:CreateTexture(nil, "BACKGROUND", nil, 0)
+	healthBar.bg:SetAllPoints(healthBar)
+	healthBar.bg:SetTexture(0.15, 0.15, 0.15)
 
-	self.health.bar.fg = self.health.bar:CreateTexture(nil, "OVERLAY", nil, 0)
-	self.health.bar.fg:SetTexture("Interface\\AddOns\\oUF_LS\\media\\nameplate")
-	self.health.bar.fg:SetTexCoord(45 / 512, 211 / 512, 3 / 64, 29 / 64)
-	self.health.bar.fg:SetPoint("CENTER")
+	healthBar.fg = healthBar:CreateTexture(nil, "OVERLAY", nil, 0)
+	healthBar.fg:SetTexture("Interface\\AddOns\\oUF_LS\\media\\nameplate")
+	healthBar.fg:SetTexCoord(319 / 512, 449 / 512, 5 / 64, 27 / 64)
+	healthBar.fg:SetPoint("TOPLEFT", -5, 5)
+	healthBar.fg:SetPoint("BOTTOMRIGHT", 5, -5)
+
+	self.health.bar = healthBar
 
 	self.health:HookScript("OnMinMaxChanged", function(self, min, max)
 		self.bar:SetMinMaxValues(min, max)
@@ -143,38 +139,55 @@ local function lsSetNamePlateStyle(self)
 		self.bar:SetStatusBarColor(lsNamePlate_GetColor(self:GetStatusBarColor()))
 	end)
 
-	-- castbar
+	-- Castbar
 	self.cast:GetStatusBarTexture():SetTexture(nil)
-	self.cast.parent = self
 
-	self.cast.bar = CreateFrame("Statusbar", nil, self.cast)
-	self.cast.bar:SetStatusBarTexture(ns.M.textures.statusbar)
+	local castBar = CreateFrame("Statusbar", nil, overlay)
+	castBar:SetStatusBarTexture(ns.M.textures.statusbar)
+	castBar:SetPoint("BOTTOM", overlay, "BOTTOM", 0, 0)
+	castBar:SetPoint("LEFT", overlay, 0, 0)
+	castBar:SetPoint("RIGHT", overlay, 0, 0)
+	castBar:SetHeight(12)
+	castBar:SetStatusBarTexture(ns.M.textures.statusbar)
+	castBar:SetStatusBarColor(0.15, 0.15, 0.15)
+	castBar:Hide()
 
-	self.cast.bar.bg = self.cast.bar:CreateTexture(nil, "BACKGROUND", nil, 0)
-	self.cast.bar.bg:SetAllPoints(self.cast.bar)
-	self.cast.bar.bg:SetTexture(0.96, 0.7, 0)
+	castBar.bg = castBar:CreateTexture(nil, "BACKGROUND", nil, 0)
+	castBar.bg:SetAllPoints(castBar)
+	castBar.bg:SetTexture(0.15, 0.15, 0.15)
 
-	self.cast.bar.fg = self.cast.bar:CreateTexture(nil, "OVERLAY", nil, 0)
-	self.cast.bar.fg:SetTexture("Interface\\AddOns\\oUF_LS\\media\\nameplate")
-	self.cast.bar.fg:SetTexCoord(45 / 512, 211 / 512, 3 / 64, 29 / 64)
-	-- self.cast.bar.fg:SetPoint("TOPLEFT", -2, 2)
-	-- self.cast.bar.fg:SetPoint("BOTTOMRIGHT", 2, -2)
+	castBar.fg = castBar:CreateTexture(nil, "OVERLAY", nil, 0)
+	castBar.fg:SetTexture("Interface\\AddOns\\oUF_LS\\media\\nameplate")
+	castBar.fg:SetTexCoord(63 / 512, 193 / 512, 5 / 64, 27 / 64)
+	castBar.fg:SetPoint("TOPLEFT", -5, 5)
+	castBar.fg:SetPoint("BOTTOMRIGHT", 5, -5)
 
-	self.cast.bar.icon = self.cast.bar:CreateTexture(nil, "BACKGROUND", nil, 1)
-	self.cast.bar.icon:SetTexCoord(0.0625, 0.9375, 0.0625, 0.9375)
+	castBar.icon = castBar:CreateTexture(nil, "BACKGROUND", nil, 1)
+	castBar.icon:SetTexCoord(0.0625, 0.9375, 0.0625, 0.9375)
+	castBar.icon:SetSize(32, 32)
+	castBar.icon:SetPoint("RIGHT", overlay, "LEFT", -8, 0)
 
-	self.cast.bar.iconborder = self.cast.bar:CreateTexture(nil, "BACKGROUND", nil, 2)
-	self.cast.bar.iconborder:SetTexture(ns.M.textures.button.normal)
-	self.cast.bar.iconborder:SetTexCoord(0.140625, 0.859375, 0.140625, 0.859375)
+	castBar.iconborder = castBar:CreateTexture(nil, "BACKGROUND", nil, 2)
+	castBar.iconborder:SetTexture(ns.M.textures.button.normal)
+	castBar.iconborder:SetTexCoord(0.140625, 0.859375, 0.140625, 0.859375)
+	castBar.iconborder:SetPoint("TOPLEFT", castBar.icon, "TOPLEFT", -4, 4)
+	castBar.iconborder:SetPoint("BOTTOMRIGHT", castBar.icon, "BOTTOMRIGHT", 4, -4)
 
-	self.cast.name:SetFont(ns.M.font, 8)
-	self.cast.name:SetParent(self.cast.bar)
-	self.cast.name:SetShadowColor(0, 0, 0)
-	self.cast.name:SetShadowOffset(1, -1)
+	self.castname:SetParent(castBar)
+	self.castname:SetFont(ns.M.font, 10)
+	self.castname:SetShadowColor(0, 0, 0)
+	self.castname:SetShadowOffset(1, -1)
+	self.castname:ClearAllPoints()
+	self.castname:SetPoint("LEFT", castBar, 2, 0)
+	self.castname:SetPoint("RIGHT", castBar, -2, 0)
+	self.castname:SetJustifyH("CENTER")
+	castBar.name = self.castname
+
+	self.cast.bar = castBar
 
 	self.cast:HookScript("OnValueChanged", function(self, value)
 		self.bar:SetValue(value)
-	
+
 		if self.shield:IsShown() then
 			self.bar.icon:SetDesaturated(true)
 			self.bar:SetStatusBarColor(0.6, 0.6, 0.6)
@@ -187,10 +200,39 @@ local function lsSetNamePlateStyle(self)
 	end)
 
 	self.cast:HookScript("OnShow", lsNamePlateCastBar_OnShow)
+	self.cast:HookScript("OnHide", lsNamePlateCastBar_OnHide)
 
-	self.name:SetFont(ns.M.font, 8)
+	-- Name
+	self.name:SetParent(overlay)
+	self.name:SetFont(ns.M.font, 12)
+	self.name:SetPoint("BOTTOM", overlay, "TOP", 0, 5)
+	self.name:SetPoint("LEFT", self.overlay, -4, 0)
+	self.name:SetPoint("RIGHT", self.overlay, 4, 0)
+
+	--RaidIcon
+	self.raid:SetParent(overlay)
+	self.raid:SetSize(32, 32)
+	self.raid:ClearAllPoints()
+	self.raid:SetPoint("LEFT", overlay, "RIGHT", 8, 0)
+
+	-- Threat
+	self.threat:SetParent(overlay)
+	self.threat:SetTexture("Interface\\AddOns\\oUF_LS\\media\\nameplate")
+	self.threat:SetTexCoord(62 / 512, 194 / 512, 36 / 64, 52 / 64)
 
 	self:HookScript("OnShow", lsNamePlate_OnShow)
+	self:HookScript("OnHide", lsNamePlate_OnHide)
+
+	self:HookScript("OnUpdate", function(self, elapsed)
+		self.elapsed = (self.elapsed or 0) + elapsed
+
+		if self.elapsed > 0.1 then
+			self.overlay:SetAlpha(self:GetAlpha())
+
+			self.elapsed = 0
+		end
+	end)
+
 	lsNamePlate_OnShow(self)
 
 	self.styled = true
@@ -204,7 +246,7 @@ function lsNamePlates_Initialize()
 		if interval > 0.1 then
 			for _, f in next, {self:GetChildren()} do
 				local name = f:GetName()
-				if name and (not f.isNotNamePlate and string.match(name, "NamePlate%d")) then
+				if not f.isNotNamePlate and (name and match(name, "^NamePlate%d")) then
 					lsSetNamePlateStyle(f)
 				else
 					f.isNotNamePlate = true
