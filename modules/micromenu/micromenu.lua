10ce8c4f073fd77f6ae781028bb29889edf03fa9
Valeriy Voronov <i.lightspark@gmail.com>
2015-02-28 01:04:18 +0700

    More logical approach on how I handle micromenu... Returned MainMenu button and removed Store one.

--- modules/micromenu/micromenu.lua
+++ modules/micromenu/micromenu.lua
@@ -1,11 +1,9 @@
 local _, ns = ...
-local E = ns.E
+local E, M = ns.E, ns.M
 
 E.MM = {}
 
 local MM = E.MM
-
-MM.Buttons = {}
 
 local MICRO_BUTTON_LAYOUT = {
 	["CharacterMicroButton"] = {
@@ -14,22 +12,22 @@ local MICRO_BUTTON_LAYOUT = {
 		icon = "character",
 	},
 	["SpellbookMicroButton"] = {
-		point = {"LEFT", "lsCharacterMicroButton", "RIGHT", 6, 0},
+		point = {"LEFT", "CharacterMicroButton", "RIGHT", 6, 0},
 		parent = "lsMBHolderLeft",
 		icon = "spellbook",
 	},
 	["TalentMicroButton"] = {
-		point = {"LEFT", "lsSpellbookMicroButton", "RIGHT", 6, 0},
+		point = {"LEFT", "SpellbookMicroButton", "RIGHT", 6, 0},
 		parent = "lsMBHolderLeft",
 		icon = "talents",
 	},
 	["AchievementMicroButton"] = {
-		point = {"LEFT", "lsTalentMicroButton", "RIGHT", 6, 0},
+		point = {"LEFT", "TalentMicroButton", "RIGHT", 6, 0},
 		parent = "lsMBHolderLeft",
 		icon = "achievement",
 	},
 	["QuestLogMicroButton"] = {
-		point = {"LEFT", "lsAchievementMicroButton", "RIGHT", 6, 0},
+		point = {"LEFT", "AchievementMicroButton", "RIGHT", 6, 0},
 		parent = "lsMBHolderLeft",
 		icon = "quest",
 	},
@@ -39,183 +37,111 @@ local MICRO_BUTTON_LAYOUT = {
 		icon = "guild",
 	},
 	["LFDMicroButton"] = {
-		point = {"LEFT", "lsGuildMicroButton", "RIGHT", 6, 0},
+		point = {"LEFT", "GuildMicroButton", "RIGHT", 6, 0},
 		parent = "lsMBHolderRight",
 		icon = "lfg",
 	},
 	["CollectionsMicroButton"] = {
-		point = {"LEFT", "lsLFDMicroButton", "RIGHT", 6, 0},
+		point = {"LEFT", "LFDMicroButton", "RIGHT", 6, 0},
 		parent = "lsMBHolderRight",
 		icon = "pet",
 	},
 	["EJMicroButton"] = {
-		point = {"LEFT", "lsCollectionsMicroButton", "RIGHT", 6, 0},
+		point = {"LEFT", "CollectionsMicroButton", "RIGHT", 6, 0},
 		parent = "lsMBHolderRight",
 		icon = "ej",
 	},
-	["StoreMicroButton"] = {
-		point = {"LEFT", "lsEJMicroButton", "RIGHT", 6, 0},
+	["MainMenuMicroButton"] = {
+		point = {"LEFT", "EJMicroButton", "RIGHT", 6, 0},
 		parent = "lsMBHolderRight",
-		icon = "store",
+		icon = "mainmenu",
 	},
 }
 
-local function UpdateMicroButtonState()
-	local playerLevel = UnitLevel("player")
-	local factionGroup = UnitFactionGroup("player")
+local function MicroButton_OnLeave(self)
+	GameTooltip:Hide()
+end
 
-	if factionGroup == "Neutral" then
-		lsGuildMicroButton.factionGroup = factionGroup
-		lsLFDMicroButton.factionGroup = factionGroup
-	else
-		lsGuildMicroButton.factionGroup = nil
-		lsLFDMicroButton.factionGroup = nil
+local function HandleMicroButton(name)
+	local button = _G[name]
+
+	local normal = button:GetNormalTexture()
+	local pushed = button:GetPushedTexture()
+	local highlight = button:GetHighlightTexture()
+	local disabled = button:GetDisabledTexture()
+	local flash = button.Flash
+
+	button:SetSize(18, 24)
+	button:SetFrameStrata("LOW")
+	button:SetFrameLevel(2)
+	button:SetHitRectInsets(0, 0, 0, 0)
+
+	if normal then normal:SetTexture(nil) end
+
+	if pushed then
+		pushed:SetTexture("Interface\\AddOns\\oUF_LS\\media\\microbutton")
+		pushed:SetTexCoord(0.7734375, 0.9140625, 0.3125, 0.6875)
+		pushed:SetSize(18, 24)
+		pushed:ClearAllPoints()
+		pushed:SetPoint("CENTER")
 	end
 
-	if CharacterFrame and CharacterFrame:IsShown() then
-		lsCharacterMicroButton:SetButtonState("PUSHED", true)
-	else
-		lsCharacterMicroButton:SetButtonState("NORMAL")
+	if highlight then
+		highlight:SetTexture("Interface\\AddOns\\oUF_LS\\media\\microbutton")
+		highlight:SetTexCoord(0.40625, 0.59375, 0.265625, 0.734375)
+		highlight:SetSize(24, 30)
+		highlight:ClearAllPoints()
+		highlight:SetPoint("CENTER")
 	end
 
-	if SpellBookFrame and SpellBookFrame:IsShown() then
-		lsSpellbookMicroButton:SetButtonState("PUSHED", true)
-	else
-		lsSpellbookMicroButton:SetButtonState("NORMAL")
+	if disabled then disabled:SetTexture(nil) end
+
+	if flash then
+		flash:SetSize(58, 58)
+		flash:ClearAllPoints()
+		flash:SetPoint("CENTER", 14, -10)
 	end
 
-	if PlayerTalentFrame and PlayerTalentFrame:IsShown() then
-		lsTalentMicroButton:SetButtonState("PUSHED", true)
-	else
-		if playerLevel < 10 then
-			lsTalentMicroButton:Disable()
+	local bg = button:CreateTexture(nil, "BACKGROUND", nil, 0)
+	bg:SetTexture(M.textures.statusbar)
+	bg:SetVertexColor(0.15, 0.15, 0.15)
+	bg:SetAllPoints()
+
+	local icon = button:CreateTexture(nil, "BACKGROUND", nil, 1)
+	icon:SetTexture("Interface\\AddOns\\oUF_LS\\media\\microicon\\"..MICRO_BUTTON_LAYOUT[name].icon)
+	icon:SetVertexColor(0.52, 0.46, 0.36)
+	icon:SetSize(24, 24)
+	icon:SetPoint("CENTER")
+
+	local border = button:CreateTexture(nil, "BORDER", nil, 0)
+	border:SetTexture("Interface\\AddOns\\oUF_LS\\media\\microbutton")
+	border:SetTexCoord(0.0625, 0.28125, 0.234375, 0.765625)
+	border:SetSize(28, 34)
+	border:SetPoint("CENTER")
+
+	button:SetScript("OnLeave", MicroButton_OnLeave)
+	button:SetScript("OnUpdate", nil)
+end
+
+local function ResetMicroButtonsParent()
+	for _, b in next, MICRO_BUTTONS do
+		local button = _G[b]
+
+		if MICRO_BUTTON_LAYOUT[b] then
+			button:SetParent(MICRO_BUTTON_LAYOUT[b].parent)
 		else
-			lsTalentMicroButton:Enable()
-			lsTalentMicroButton:SetButtonState("NORMAL")
+			button:SetParent(M.hiddenParent)
 		end
-	end
-
-	if AchievementFrame and AchievementFrame:IsShown() then
-		lsAchievementMicroButton:SetButtonState("PUSHED", true)
-	else
-		if (HasCompletedAnyAchievement() or IsInGuild()) and CanShowAchievementUI() then
-			lsAchievementMicroButton:Enable()
-			lsAchievementMicroButton:SetButtonState("NORMAL")
-		else
-			lsAchievementMicroButton:Disable()
-		end
-	end
-
-	if WorldMapFrame and WorldMapFrame:IsShown() then
-		lsQuestLogMicroButton:SetButtonState("PUSHED", true)
-	else
-		lsQuestLogMicroButton:SetButtonState("NORMAL")
-	end
-
-	if IsTrialAccount() or (IsVeteranTrialAccount() and not IsInGuild()) or factionGroup == "Neutral" then
-		lsGuildMicroButton:Disable()
-	elseif (GuildFrame and GuildFrame:IsShown()) or (LookingForGuildFrame and LookingForGuildFrame:IsShown()) then
-		lsGuildMicroButton:Enable()
-		lsGuildMicroButton:SetButtonState("PUSHED", true)
-	else
-		lsGuildMicroButton:Enable()
-		lsGuildMicroButton:SetButtonState("NORMAL")
-		if IsInGuild() then
-			lsGuildMicroButton.tooltipText = MicroButtonTooltipText(GUILD, "TOGGLEGUILDTAB")
-		else
-			lsGuildMicroButton.tooltipText = MicroButtonTooltipText(LOOKINGFORGUILD, "TOGGLEGUILDTAB")
-		end
-	end
-
-	if PVEFrame and PVEFrame:IsShown() then
-		lsLFDMicroButton:SetButtonState("PUSHED", true)
-	else
-		if playerLevel < lsLFDMicroButton.minLevel or factionGroup == "Neutral" then
-			lsLFDMicroButton:Disable()
-		else
-			lsLFDMicroButton:Enable()
-			lsLFDMicroButton:SetButtonState("NORMAL")
-		end
-	end
-
-	if CollectionsJournal and CollectionsJournal:IsShown() then
-		lsCollectionsMicroButton:SetButtonState("PUSHED", true)
-	else
-		lsCollectionsMicroButton:SetButtonState("NORMAL")
-	end
-
-	if EncounterJournal and EncounterJournal:IsShown() then
-		lsEJMicroButton:SetButtonState("PUSHED", true)
-	else
-		lsEJMicroButton:SetButtonState("NORMAL")
-	end
-
-	if GameLimitedMode_IsActive() then
-		lsStoreMicroButton.disabledTooltip = GameLimitedMode_GetString("ERR_FEATURE_RESTRICTED")
-		lsStoreMicroButton:Disable()
-	elseif C_StorePublic.IsDisabledByParentalControls() then
-		lsStoreMicroButton.disabledTooltip = BLIZZARD_STORE_ERROR_PARENTAL_CONTROLS
-		lsStoreMicroButton:Disable()
-	else
-		lsStoreMicroButton.disabledTooltip = nil
-		lsStoreMicroButton:Enable()
-	end
-
-	if StoreFrame and StoreFrame_IsShown() then
-		lsStoreMicroButton:SetButtonState("PUSHED", true)
-	else
-		lsStoreMicroButton:SetButtonState("NORMAL")
 	end
 end
 
-local function MicroButton_OnEvent(self, event)
-	local name = self:GetName()
+local function ResetMicroButtonsPosition()
+	for _, b in next, MICRO_BUTTONS do
+		local button = _G[b]
 
-	if event == "UPDATE_BINDINGS" or event == "CUSTOM_FORCE_UPDATE" then
-		if name == "lsCharacterMicroButton" then
-			self.tooltipText = MicroButtonTooltipText(CHARACTER_BUTTON, "TOGGLECHARACTER0")
-		elseif name == "lsSpellbookMicroButton" then
-			self.tooltipText = MicroButtonTooltipText(SPELLBOOK_ABILITIES_BUTTON, "TOGGLESPELLBOOK")
-		elseif name == "lsTalentMicroButton" then
-			self.tooltipText = MicroButtonTooltipText(TALENTS_BUTTON, "TOGGLETALENTS")
-		elseif name == "lsAchievementMicroButton" then
-			self.tooltipText = MicroButtonTooltipText(ACHIEVEMENT_BUTTON, "TOGGLEACHIEVEMENT")
-		elseif name == "lsQuestLogMicroButton" then
-			self.tooltipText = MicroButtonTooltipText(QUESTLOG_BUTTON, "TOGGLEQUESTLOG")
-		elseif name == "lsGuildMicroButton" then
-			if IsInGuild() then
-				self.tooltipText = MicroButtonTooltipText(GUILD, "TOGGLEGUILDTAB")
-			else
-				self.tooltipText = MicroButtonTooltipText(LOOKINGFORGUILD, "TOGGLEGUILDTAB")
-			end
-		elseif name == "lsLFDMicroButton" then
-			self.tooltipText = MicroButtonTooltipText(DUNGEONS_BUTTON, "TOGGLEGROUPFINDER")
-		elseif name == "lsCollectionsMicroButton" then
-			self.tooltipText = MicroButtonTooltipText(COLLECTIONS, "TOGGLECOLLECTIONS")
-		elseif name == "lsEJMicroButton" then
-			self.tooltipText = MicroButtonTooltipText(ENCOUNTER_JOURNAL, "TOGGLEENCOUNTERJOURNAL")
-		elseif name == "lsStoreMicroButton" then
-			self.tooltipText = BLIZZARD_STORE
-		end
-	end
-
-	UpdateMicroButtonState()
-end
-
-local function InitializeMicroButton(self, events, level, isBlizzConDriven, isTrialDriven)
-	if events then
-		for i = 1, #events do
-			self:RegisterEvent(events[i])
-		end
-	end
-
-	if level then self.minLevel = level end
-
-	if isTrialDriven then
-		if IsTrialAccount() then
-			self:Disable()
-			self:SetAlpha(0.5)
-			self.disabledTooltip = ERR_RESTRICTED_ACCOUNT
+		if MICRO_BUTTON_LAYOUT[b] then
+			button:ClearAllPoints()
+			button:SetPoint(unpack(MICRO_BUTTON_LAYOUT[b].point))
 		end
 	end
 end
@@ -235,56 +161,35 @@ function MM:Initialize()
 	holder2:SetSize(18 * 5 + 6 * 5, 24 + 6)
 	holder2:SetPoint(unpack(MM_CONFIG.holder2.point))
 
-	for mb, data in next, MICRO_BUTTON_LAYOUT do
-		local button = CreateFrame("Button", "ls"..mb, _G[data.parent], "lsMicroButtonTemplate")
-		button:SetFrameStrata("LOW")
-		button:SetFrameLevel(2)
+	for _, b in next, MICRO_BUTTONS do
+		local button = _G[b]
+		if MICRO_BUTTON_LAYOUT[b] then
+			HandleMicroButton(b)
 
-		button.icon:SetTexture("Interface\\AddOns\\oUF_LS\\media\\microicon\\"..data.icon)
+			button:SetParent(MICRO_BUTTON_LAYOUT[b].parent)
+			button:ClearAllPoints()
+			button:SetPoint(unpack(MICRO_BUTTON_LAYOUT[b].point))
+		else
+			button:UnregisterAllEvents()
+			button:SetParent(M.hiddenParent)
+		end
 
-		button.MicroButton_OnEvent = MicroButton_OnEvent
+		if b == "CharacterMicroButton" then
+			E:AlwaysHide(MicroButtonPortrait)
+		elseif b == "MainMenuMicroButton" then
+			E:AlwaysHide(MainMenuBarPerformanceBar)
+			E:AlwaysHide(MainMenuBarDownload)
 
-		MM.Buttons[mb] = button
+			button:SetScript("OnEnter", MicroButton_OnEnter)
+		end
 	end
 
-	lsCharacterMicroButton:SetScript("OnClick", function(...) ToggleCharacter("PaperDollFrame") end)
+	TalentMicroButtonAlert:SetPoint("BOTTOM", "TalentMicroButton", "TOP", 0, 12)
 
-	lsSpellbookMicroButton:SetScript("OnClick", function(...) ToggleFrame(SpellBookFrame) end)
+	CollectionsMicroButtonAlert:SetPoint("BOTTOM", "CollectionsMicroButton", "TOP", 0, 12)
 
-	InitializeMicroButton(lsTalentMicroButton, {"PLAYER_LEVEL_UP", "PLAYER_TALENT_UPDATE"}, 10, true)
-	lsTalentMicroButton:SetScript("OnClick", function(...) ToggleTalentFrame() end)
-	TalentMicroButtonAlert:SetPoint("BOTTOM", lsTalentMicroButton, "TOP", 0, 12)
+	LFDMicroButtonAlert:SetPoint("BOTTOM", "LFDMicroButton", "TOP", 0, 12)
 
-	InitializeMicroButton(lsAchievementMicroButton, {"RECEIVED_ACHIEVEMENT_LIST", "ACHIEVEMENT_EARNED"}, 10)
-	lsAchievementMicroButton:SetScript("OnClick", function(...) ToggleAchievementFrame() end)
-
-	lsQuestLogMicroButton:SetScript("OnClick", function(...) ToggleQuestLog() end)
-
-	InitializeMicroButton(lsGuildMicroButton, {"PLAYER_GUILD_UPDATE", "NEUTRAL_FACTION_SELECT_RESULT"}, nil, true, true)
-	lsGuildMicroButton:SetScript("OnClick", function(...) ToggleGuildFrame() end)
-
-	InitializeMicroButton(lsLFDMicroButton, nil, 10, true)
-	lsLFDMicroButton:SetScript("OnClick", function(...) PVEFrame_ToggleFrame() end)
-	LFDMicroButtonAlert:SetPoint("BOTTOM", lsLFDMicroButton, "TOP", 0, 12)
-
-	lsCollectionsMicroButton:SetScript("OnClick", function(...) ToggleCollectionsJournal() end)
-	CollectionsMicroButtonAlert:SetPoint("BOTTOM", lsCollectionsMicroButton, "TOP", 0, 12)
-
-	InitializeMicroButton(lsEJMicroButton, nil, 15, true)
-	lsEJMicroButton:SetScript("OnClick", function(...) ToggleEncounterJournal() end)
-
-	InitializeMicroButton(lsStoreMicroButton, {"STORE_STATUS_CHANGED"})
-	lsStoreMicroButton:SetScript("OnClick", function(...) ToggleStoreUI() end)
-
-	for mb, button in next, MM.Buttons do
-		button:SetPoint(unpack(MICRO_BUTTON_LAYOUT[mb].point))
-		button:MicroButton_OnEvent("CUSTOM_FORCE_UPDATE")
-	end
-
-	for _, f in next, MICRO_BUTTONS do
-		_G[f]:UnregisterAllEvents()
-		_G[f]:SetParent(ns.M.hiddenParent)
-	end
-
-	hooksecurefunc("UpdateMicroButtons", UpdateMicroButtonState)
+	hooksecurefunc("UpdateMicroButtonsParent", ResetMicroButtonsParent)
+	hooksecurefunc("MoveMicroButtons", ResetMicroButtonsPosition)
 end
