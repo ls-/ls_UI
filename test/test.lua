4e5d264656a9e037beab52284fe6a58266c041bc
Valeriy Voronov <i.lightspark@gmail.com>
2015-04-14 20:43:22 +0700

    meh...

--- test/test.lua
+++ test/test.lua
@@ -1,5 +1,6 @@
 local AddOn, ns = ...
 local E, M = ns.E, ns.M
 
-print("Val! Current TOC number is", select(4, GetBuildInfo()))
-print("oUF: LS", GetAddOnMetadata(AddOn, "Version"))
+-- print("Val! Current TOC number is", select(4, GetBuildInfo()))
+-- print("oUF: LS", GetAddOnMetadata(AddOn, "Version"))
+print("Welcome to oUF: |cff1a9fc0LS|r "..GetAddOnMetadata(AddOn, "Version")..". To open config panel use |cffffbf19/lsconfig|r command.")
