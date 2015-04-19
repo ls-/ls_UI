892ce8aa9b7cfdc1d8ad722fc56fa6dc4dc25147
Valeriy Voronov <i.lightspark@gmail.com>
2015-04-19 23:56:43 +0700

    Rewrote oUF_DebuffHighlight plugin.

--- embeds/oUF_DebuffHighlight/oUF_DebuffHighlight.lua
+++ embeds/oUF_DebuffHighlight/oUF_DebuffHighlight.lua
@@ -1,161 +1,157 @@
-if not oUF then return end
+local _, ns = ...
+local oUF = ns.oUF or oUF
+assert(oUF, "oUF Debuff Highlight was unable to locate oUF install")
 
-local playerClass = select(2,UnitClass("player"))
-local CanDispel = {
-  PRIEST = { Magic = false, Disease = false, },
-  SHAMAN = { Magic = false, Curse = false, },
-  PALADIN = { Magic = false, Poison = false, Disease = false, },
-  MAGE = { Curse = false, },
-  DRUID = { Magic = false, Curse = false, Poison = false, },
-  MONK = { Magic = false, Poison = false, Disease = false, }
+local _, PlayerClass = UnitClass("player")
+
+local DISPELTYPES = {
+	PALADIN = {Magic = false, Disease = false, Poison = false},
+	SHAMAN = {Magic = false, Curse = false},
+	DRUID = {Magic = false, Curse = false, Poison = false},
+	PRIEST = {Magic = false, Disease = false},
+	MONK = {Magic = false, Disease = false, Poison = false},
+	MAGE = {Curse = false},
 }
 
-local dispellist = CanDispel[playerClass] or {}
-local origColors = {}
-local origBorderColors = {}
-local origPostUpdateAura = {}
+local CanDispel = DISPELTYPES[PlayerClass]
+local OriginalColors = {}
+local DebuffTypeColor = DebuffTypeColor
 
 local function GetDebuffType(unit, filter)
-  if not UnitCanAssist("player", unit) then return nil end
-  local i = 1
-  while true do
-    local _, _, texture, _, debufftype = UnitAura(unit, i, "HARMFUL")
-    if not texture then break end
-    if debufftype and not filter or (filter and dispellist[debufftype]) then
-      return debufftype, texture
-    end
-    i = i + 1
-  end
+	if not UnitCanAssist("player", unit) then return nil end
+
+	local i = 1
+	local name, debuffType
+	while true do
+		name, _, _, _, debuffType = UnitAura(unit, i, "HARMFUL")
+
+		if not name then break end
+
+		if debuffType and not filter or (filter and CanDispel[debuffType]) then
+			return debuffType, DebuffTypeColor[debuffType]
+		end
+
+		i = i + 1
+	end
 end
 
+local function CheckDispelTypes(self, event)
+	for k, v in pairs(CanDispel) do
+		CanDispel[k] = false
+	end
 
-local function CheckSpec(self, event, levels)
-  local spec = GetSpecialization()
-  local slevel = UnitLevel("player")
+	if PlayerClass == "PALADIN" then
+		if IsPlayerSpell(4987) then -- Cleanse
+			CanDispel.Poison = true
+			CanDispel.Disease = true
 
-  if playerClass == "PALADIN"  and slevel > 19 then -- Cleanse
-    dispellist.Poison = true
-    dispellist.Disease = true
-    if (spec == 1) then               -- Sacred Cleansing
-      dispellist.Magic = true
-    else
-      dispellist.Magic = false
-    end
-  elseif playerClass == "SHAMAN" and slevel > 17 then -- Cleanse Spirit
-    dispellist.Curse = true
-    if (spec == 3) then               -- Purify Spirit
-      dispellist.Magic = true
-    else
-      dispellist.Magic = false
-    end
-  elseif playerClass == "DRUID" and slevel > 21 then  -- Remove Corruption
-    dispellist.Poison = true
-    dispellist.Curse = true
-    if (spec == 4) then               -- Nature's Cure
-      dispellist.Magic = true
-    else
-      dispellist.Magic = false
-    end
-  elseif playerClass == "PRIEST" and slevel > 21 then
-    if not (spec == 3) then             -- Purify
-      dispellist.Magic = true
-      dispellist.Disease = true
-    else
-      if slevel > 71 then             -- Mass Dispel
-        dispellist.Magic = true
-      else
-        dispellist.Magic = false
-      end
-      dispellist.Disease = false
-    end
-  elseif playerClass == "MONK" and slevel > 19 then -- Detox
-    dispellist.Poison = true
-    dispellist.Disease = true
-    if (spec == 2) then               -- Internal Medicine
-      dispellist.Magic = true
-    else
-      dispellist.Magic = false
-    end
-  elseif playerClass == "MAGE" and slevel > 28 then -- Remove Curse
-    dispellist.Curse = true
-  end
+			if IsPlayerSpell(53551) then -- Sacred Cleansing
+				CanDispel.Magic = true
+			end
+		end
+	elseif PlayerClass == "SHAMAN" then
+		if IsPlayerSpell(51886) then -- Cleanse Spirit
+			CanDispel.Curse = true
+		elseif IsPlayerSpell(77130) then -- Purify Spirit
+			CanDispel.Curse = true
+			CanDispel.Magic = true
+		end
+	elseif PlayerClass == "DRUID" then
+		if IsPlayerSpell(2782) then -- Remove Corruption
+			CanDispel.Curse = true
+			CanDispel.Poison = true
+		elseif IsPlayerSpell(88423) then -- Nature's Cure
+			CanDispel.Magic = true
+			CanDispel.Curse = true
+			CanDispel.Poison = true
+		end
+	elseif PlayerClass == "PRIEST"  then
+		if IsPlayerSpell(527) then -- Purify
+			CanDispel.Magic = true
+			CanDispel.Disease = true
+		elseif IsPlayerSpell(32375) then -- Mass Dispel
+			if IsPlayerSpell(55691) then						 
+				CanDispel.Magic = true
+			end
+
+			CanDispel.Disease = false
+		end
+	elseif PlayerClass == "MONK" then
+		if IsPlayerSpell(115450) then -- Detox
+			CanDispel.Poison = true
+			CanDispel.Disease = true
+
+			if IsPlayerSpell(115451) then -- Internal Medicine
+				CanDispel.Magic = true
+			end
+		elseif IsPlayerSpell(115310) then -- Revival
+			CanDispel.Poison = true
+			CanDispel.Disease = true
+			CanDispel.Magic = true
+		end
+	elseif PlayerClass == "MAGE" then
+		if IsPlayerSpell(475) then -- Remove Curse
+			CanDispel.Curse = true
+		end
+	end
 end
 
-local function Update(object, event, unit)
-  if object.unit ~= unit  then return end
-  local debuffType, texture  = GetDebuffType(unit, object.DebuffHighlightFilter)
-  if debuffType then
-    local color = DebuffTypeColor[debuffType]
-    if object.DebuffHighlightBackdrop or object.DebuffHighlightBackdropBorder then
-      if object.DebuffHighlightBackdrop then
-        object:SetBackdropColor(color.r, color.g, color.b, object.DebuffHighlightAlpha or 1)
-      end
-      if object.DebuffHighlightBackdropBorder then
-        object:SetBackdropBorderColor(color.r, color.g, color.b, object.DebuffHighlightAlpha or 1)
-      end
-    elseif object.DebuffHighlightUseTexture then
-      object.DebuffHighlight:SetTexture(texture)
-    else
-      object.DebuffHighlight:SetVertexColor(color.r, color.g, color.b, object.DebuffHighlightAlpha or .5)
-    end
-  else
-    if object.DebuffHighlightBackdrop or object.DebuffHighlightBackdropBorder then
-      local color
-      if object.DebuffHighlightBackdrop then
-        color = origColors[object]
-        object:SetBackdropColor(color.r, color.g, color.b, color.a)
-      end
-      if object.DebuffHighlightBackdropBorder then
-        color = origBorderColors[object]
-        object:SetBackdropBorderColor(color.r, color.g, color.b, color.a)
-      end
-    elseif object.DebuffHighlightUseTexture then
-      object.DebuffHighlight:SetTexture(nil)
-    else
-      local color = origColors[object]
-      object.DebuffHighlight:SetVertexColor(color.r, color.g, color.b, color.a)
-    end
-  end
+local function Update(self, event, unit)
+	if self.unit ~= unit then return end
+
+	local dbh = self.DebuffHighlight
+	local debuffType, color = GetDebuffType(unit, dbh.Filter)
+
+	if debuffType then
+		dbh:SetVertexColor(color.r, color.g, color.b, dbh.Alpha or .5)
+	else
+		color = OriginalColors[self]
+		dbh:SetVertexColor(color.r, color.g, color.b, color.a)
+	end
 end
 
-local function Enable(object)
-  -- if we're not highlighting this unit return
-  if not object.DebuffHighlightBackdrop and not object.DebuffHighlightBackdropBorder and not object.DebuffHighlight then
-    return
-  end
-  -- if we're filtering highlights and we're not of the dispelling type, return
-  if object.DebuffHighlightFilter and not CanDispel[playerClass] then
-    return
-  end
-
-  -- make sure aura scanning is active for this object
-  object:RegisterEvent("UNIT_AURA", Update)
-  object:RegisterEvent("PLAYER_TALENT_UPDATE", CheckSpec)
-  object:RegisterEvent("CHARACTER_POINTS_CHANGED", CheckSpec)
-  CheckSpec()
-
-  if object.DebuffHighlightBackdrop or object.DebuffHighlightBackdropBorder then
-    local r, g, b, a = object:GetBackdropColor()
-    origColors[object] = { r = r, g = g, b = b, a = a}
-    r, g, b, a = object:GetBackdropBorderColor()
-    origBorderColors[object] = { r = r, g = g, b = b, a = a}
-  elseif not object.DebuffHighlightUseTexture then -- color debuffs
-    -- object.DebuffHighlight
-    local r, g, b, a = object.DebuffHighlight:GetVertexColor()
-    origColors[object] = { r = r, g = g, b = b, a = a}
-  end
-
-  return true
+local function Path(self, ...)
+	return (self.DebuffHighlight.Override or Update) (self, ...)
 end
 
-local function Disable(object)
-  if object.DebuffHighlightBackdrop or object.DebuffHighlightBackdropBorder or object.DebuffHighlight then
-    object:UnregisterEvent("UNIT_AURA", Update)
-    object:UnregisterEvent("PLAYER_TALENT_UPDATE", CheckSpec)
-    object:UnregisterEvent("CHARACTER_POINTS_CHANGED", CheckSpec)
-  end
+local function ForceUpdate(element)
+	return Path(element.__owner, "ForceUpdate", element.__owner.unit)
 end
 
-oUF:AddElement('DebuffHighlight', Update, Enable, Disable)
+local function Enable(self)
+	local dbh = self.DebuffHighlight
 
-for i, frame in ipairs(oUF.objects) do Enable(frame) end
+	if not dbh then return end
+
+	if dbh.Filter and not DISPELTYPES[PlayerClass] then
+		return
+	end
+
+	if dbh:IsObjectType("Texture") and not dbh:GetTexture() then
+		print("No texture is set for debuff highlight. DBH is disabled for ", self:GetName())
+	end
+
+	dbh.__owner = self
+	dbh.ForceUpdate = ForceUpdate
+
+	self:RegisterEvent("UNIT_AURA", Path)
+	self:RegisterEvent("SPELLS_CHANGED", CheckDispelTypes)
+	self:RegisterEvent("PLAYER_TALENT_UPDATE", CheckDispelTypes)
+
+	local r, g, b, a = dbh:GetVertexColor()
+	OriginalColors[self] = {r = r, g = g, b = b, a = a}
+
+	return true
+end
+
+local function Disable(self)
+	local dbh = self.DebuffHighlight
+
+	if not dbh then return end
+
+	self:UnregisterEvent("UNIT_AURA", Path)
+	self:UnregisterEvent("SPELLS_CHANGED", CheckDispelTypes)
+	self:UnregisterEvent("PLAYER_TALENT_UPDATE", CheckDispelTypes)
+end
+
+oUF:AddElement('DebuffHighlight', Path, Enable, Disable)
