6228a7a6a729cd6df10efbe09d717ad0ba426cb3
Valeriy Voronov <i.lightspark@gmail.com>
2015-05-10 06:17:56 +0700

    Added mover for PlayerPowerBarAlt.

---
+++ modules/blizzard/powerbaralt.lua
@@ -0,0 +1,18 @@
+local _, ns = ...
+local E, M = ns.E, ns.M
+
+local B = E.Blizzard
+
+function B:HandlePowerBarAlt()
+	local holder = CreateFrame("Frame", "LSPowerBarAltHolder", UIParent)
+	holder:SetFrameLevel(1)
+	holder:SetSize(64, 64)
+	holder:SetPoint("BOTTOM", 0, 230)
+
+	E:CreateMover(holder)
+
+	PlayerPowerBarAlt:ClearAllPoints()
+	PlayerPowerBarAlt:SetParent(holder)
+	PlayerPowerBarAlt:SetPoint("BOTTOM", holder, "BOTTOM", 0, 0)
+	PlayerPowerBarAlt.ignoreFramePositionManager = true
+end
