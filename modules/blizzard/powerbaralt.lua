e7d65c8e2d9ce8ad38bdf997602d6cdd1b744ab8
Valeriy Voronov <i.lightspark@gmail.com>
2015-06-09 16:17:55 +0700

    Blizz, y u do dis?

--- modules/blizzard/powerbaralt.lua
+++ modules/blizzard/powerbaralt.lua
@@ -3,14 +3,20 @@ local E, M = ns.E, ns.M
 
 local B = E.Blizzard
 
+local function ClearAllPointsHook(self)
+	self:SetPoint("BOTTOM", LSPowerBarAltHolder, "BOTTOM", 0, 0)
+end
+
 function B:HandlePowerBarAlt()
 	local holder = CreateFrame("Frame", "LSPowerBarAltHolder", UIParent)
 	holder:SetSize(64, 64)
 	holder:SetPoint("BOTTOM", 0, 230)
 	E:CreateMover(holder)
 
-	PlayerPowerBarAlt:ClearAllPoints()
 	PlayerPowerBarAlt:SetParent(holder)
+	PlayerPowerBarAlt:ClearAllPoints()
 	PlayerPowerBarAlt:SetPoint("BOTTOM", holder, "BOTTOM", 0, 0)
 	PlayerPowerBarAlt.ignoreFramePositionManager = true
+
+	hooksecurefunc(PlayerPowerBarAlt, "ClearAllPoints", ClearAllPointsHook)
 end
