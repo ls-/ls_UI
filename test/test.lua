6e445d87f71712eeae951e48b3169c91acc78509
Valeriy Voronov <i.lightspark@gmail.com>
2015-07-29 21:29:46 +0700

    People dun need it anyway.

--- test/test.lua
+++ test/test.lua
@@ -3,4 +3,4 @@ local E, M = ns.E, ns.M
 
 -- print("Val! Current TOC number is", select(4, GetBuildInfo()))
 -- print("oUF: LS", GetAddOnMetadata(AddOn, "Version"))
-print("Welcome to oUF: |cff1a9fc0LS|r "..GetAddOnMetadata(AddOn, "Version")..". To open config panel use |cffffbf19/lsconfig|r command.")
+-- print("Welcome to oUF: |cff1a9fc0LS|r "..GetAddOnMetadata(AddOn, "Version")..". To open config panel use |cffffbf19/lsconfig|r command.")
