222397c3dcef8428e78bea1c374424b24b16b6b3
Valeriy Voronov <i.lightspark@gmail.com>
2015-11-02 14:50:09 +0700

    A bit more failsafe.

--- modules/tools/misc.lua
+++ modules/tools/misc.lua
@@ -14,44 +14,49 @@ end
 function E:GetUnitReactionColor(unit)
 	local color
 	if unit then
-		color = COLORS.reaction[UnitReaction(unit, "player") or 4]
-
-		return {r = color[1], g = color[2], b = color[3], hex = E:RGBToHEX(color)}
-	else
-		color = COLORS.reaction[4] -- use Neutral faction colour by default
-
-		return {r = color[1], g = color[2], b = color[3], hex = E:RGBToHEX(color)}
+		color = COLORS.reaction[UnitReaction(unit, "player")]
 	end
+
+	color = color or COLORS.reaction[4] -- use Neutral faction colour by default
+
+	return {r = color[1], g = color[2], b = color[3], hex = E:RGBToHEX(color)}
 end
 
 function E:GetUnitClassColor(unit)
-	if not unit then return end
+	local color
 
-	local _, class = UnitClass(unit)
-	local color = COLORS.class[class]
+	if unit then
+		local _, class = UnitClass(unit)
+		color = COLORS.class[class]
+	end
+
+	color = color or COLORS.reaction[4]
 
 	return {r = color[1], g = color[2], b = color[3], hex = E:RGBToHEX(color)}
 end
 
 function E:GetSmartReactionColor(unit)
-	if not unit then return end
-
 	local color
-	if UnitIsDeadOrGhost(unit) or not UnitIsConnected(unit) then
-		color = COLORS.disconnected
-	elseif UnitIsPlayer(unit) then
-		color = COLORS.class[select(2, UnitClass(unit))]
-	elseif not UnitIsUnit(unit, "player") and UnitIsTapped(unit) and not UnitIsTappedByPlayer(unit) then
-		color = COLORS.tapped
-	else
-		color = COLORS.reaction[UnitReaction(unit, "player") or 4]
+
+	if unit then
+		if UnitIsDeadOrGhost(unit) or not UnitIsConnected(unit) then
+			color = COLORS.disconnected
+		elseif UnitIsPlayer(unit) then
+			color = COLORS.class[select(2, UnitClass(unit))]
+		elseif not UnitIsUnit(unit, "player") and UnitIsTapped(unit) and not UnitIsTappedByPlayer(unit) then
+			color = COLORS.tapped
+		else
+			color = COLORS.reaction[UnitReaction(unit, "player")]
+		end
 	end
+
+	color = color or COLORS.reaction[4]
 
 	return {r = color[1], g = color[2], b = color[3], hex = E:RGBToHEX(color)}
 end
 
 function E:GetUnitClassification(unit)
-	if not unit then return end
+	if not unit then return "" end
 
 	local c = UnitClassification(unit)
 
