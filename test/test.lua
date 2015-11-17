56da1c3f265fe9af0669adc072aa87b4167b25a5
Valeriy Voronov <i.lightspark@gmail.com>
2015-11-17 15:09:27 +0700

    A bit more logical... As if anyone cares, no one ever sees what I do there :D

--- test/test.lua
+++ test/test.lua
@@ -1,6 +1,2 @@
 local AddOn, ns = ...
 local E, C, M, L = ns.E, ns.C, ns.M, ns.L
-
--- print("Val! Current TOC number is", select(4, GetBuildInfo()))
--- print("oUF: LS", GetAddOnMetadata(AddOn, "Version"))
--- print("Welcome to oUF: |cff1a9fc0LS|r "..GetAddOnMetadata(AddOn, "Version")..". To open config panel use |cffffbf19/lsconfig|r command.")
