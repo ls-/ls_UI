2dfcbfc166d7fc364675ba45748ac6cc727ce8b5
Val Voronov <i.lightspark@gmail.com>
2016-04-08 18:57:52 +0700

    Tweaked utility functions a bit.

--- modules/tools/misc.lua
+++ modules/tools/misc.lua
@@ -1,14 +1,15 @@
 local _, ns = ...
 local E, C, M, L = ns.E, ns.C, ns.M, ns.L
-local COLORS = M.colors
 
+-- Lua
 local _G = _G
 local pairs, tonumber, select = pairs, tonumber, select
 local strupper, strgsub, strmatch = string.upper, string.gsub, string.match
 local mfloor = math.floor
 
-local _, playerClass = UnitClass("player")
-local playerSpec = GetSpecialization() or 0
+-- Mine
+local _, playerClass = _G.UnitClass("player")
+local playerSpec = _G.GetSpecialization() or 0
 local playerRole
 
 local ScanTooltip = _G.CreateFrame("GameTooltip", "LSiLevelScanTooltip", nil, "GameTooltipTemplate")
@@ -23,12 +24,12 @@ local dispelTypesByClass = {
 	MAGE = {},
 }
 
-local ITEM_LEVEL_PATTERN = gsub(ITEM_LEVEL, "%%d", "(%%d+)")
+local ITEM_LEVEL_PATTERN = strgsub(ITEM_LEVEL, "%%d", "(%%d+)")
 
 local INSPECT_ILVL_SLOTS = {1, 2, 3, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17}
 
 function E:GetCreatureDifficultyColor(level)
-	local color = GetCreatureDifficultyColor(level > 0 and level or 199)
+	local color = _G.GetCreatureDifficultyColor(level > 0 and level or 199)
 
 	return {r = color.r, g = color.g, b = color.b, hex = E:RGBToHEX(color)}
 end
@@ -36,10 +37,10 @@ end
 function E:GetUnitReactionColor(unit)
 	local color
 	if unit then
-		color = COLORS.reaction[UnitReaction(unit, "player")]
+		color = M.colors.reaction[_G.UnitReaction(unit, "player")]
 	end
 
-	color = color or COLORS.reaction[4] -- use Neutral faction colour by default
+	color = color or M.colors.reaction[4] -- use Neutral faction colour by default
 
 	return {r = color[1], g = color[2], b = color[3], hex = E:RGBToHEX(color)}
 end
@@ -48,11 +49,11 @@ function E:GetUnitClassColor(unit)
 	local color
 
 	if unit then
-		local _, class = UnitClass(unit)
-		color = COLORS.class[class]
+		local _, class = _G.UnitClass(unit)
+		color = M.colors.class[class]
 	end
 
-	color = color or COLORS.reaction[4]
+	color = color or M.colors.reaction[4]
 
 	return {r = color[1], g = color[2], b = color[3], hex = E:RGBToHEX(color)}
 end
@@ -61,18 +62,18 @@ function E:GetSmartReactionColor(unit)
 	local color
 
 	if unit then
-		if UnitIsDeadOrGhost(unit) or not UnitIsConnected(unit) then
-			color = COLORS.disconnected
-		elseif UnitIsPlayer(unit) then
-			color = COLORS.class[select(2, UnitClass(unit))]
-		elseif not UnitIsUnit(unit, "player") and UnitIsTapped(unit) and not UnitIsTappedByPlayer(unit) then
-			color = COLORS.tapped
+		if _G.UnitIsDeadOrGhost(unit) or not _G.UnitIsConnected(unit) then
+			color = M.colors.disconnected
+		elseif _G.UnitIsPlayer(unit) then
+			color = M.colors.class[select(2, _G.UnitClass(unit))]
+		elseif not _G.UnitIsUnit(unit, "player") and _G.UnitIsTapped(unit) and not _G.UnitIsTappedByPlayer(unit) then
+			color = M.colors.tapped
 		else
-			color = COLORS.reaction[UnitReaction(unit, "player")]
+			color = M.colors.reaction[_G.UnitReaction(unit, "player")]
 		end
 	end
 
-	color = color or COLORS.reaction[4]
+	color = color or M.colors.reaction[4]
 
 	return {r = color[1], g = color[2], b = color[3], hex = E:RGBToHEX(color)}
 end
@@ -80,7 +81,7 @@ end
 function E:GetUnitClassification(unit)
 	if not unit then return "" end
 
-	local c = UnitClassification(unit)
+	local c = _G.UnitClassification(unit)
 
 	if c == "rare" then
 		return "R"
@@ -98,11 +99,11 @@ function E:GetUnitClassification(unit)
 end
 
 function E:GetUnitPVPStatus(unit)
-	local faction = UnitFactionGroup(unit)
+	local faction = _G.UnitFactionGroup(unit)
 
-	if UnitIsPVPFreeForAll(unit) then
+	if _G.UnitIsPVPFreeForAll(unit) then
 		return true, "FFA"
-	elseif UnitIsPVP(unit) and faction and faction ~= "Neutral" then
+	elseif _G.UnitIsPVP(unit) and faction and faction ~= "Neutral" then
 		return true, strupper(faction)
 	else
 		return
@@ -122,7 +123,7 @@ function E:GetPlayerSpecFlag()
 end
 
 function E:GetPlayerRole(spec)
-	local _, _, _, _, _, role = GetSpecializationInfo(playerSpec)
+	local _, _, _, _, _, role = _G.GetSpecializationInfo(playerSpec)
 	return role or "DAMAGER"
 end
 
@@ -130,7 +131,7 @@ function E:PLAYER_SPECIALIZATION_CHANGED()
 	local oldSpec = playerSpec
 	local oldRole = playerRole
 
-	playerSpec = GetSpecialization() or 0
+	playerSpec = _G.GetSpecialization() or 0
 	playerRole = E:GetPlayerRole(playerSpec)
 
 	-- if oldSpec ~= playerSpec then
@@ -151,25 +152,25 @@ function E:SPELLS_CHANGED(...)
 
 	if dispelTypes then
 		if playerClass == "PALADIN" then
-			dispelTypes.Disease = IsPlayerSpell(4987) or nil -- Cleanse
-			dispelTypes.Magic = IsPlayerSpell(53551) or nil -- Sacred Cleansing
+			dispelTypes.Disease = _G.IsPlayerSpell(4987) or nil -- Cleanse
+			dispelTypes.Magic = _G.IsPlayerSpell(53551) or nil -- Sacred Cleansing
 			dispelTypes.Poison = dispelTypes.Disease
 		elseif playerClass == "SHAMAN" then
-			dispelTypes.Curse = IsPlayerSpell(51886) or IsPlayerSpell(77130) or nil -- Cleanse Spirit or Purify Spirit
-			dispelTypes.Magic = IsPlayerSpell(77130) or nil -- Purify Spirit
+			dispelTypes.Curse = _G.IsPlayerSpell(51886) or _G.IsPlayerSpell(77130) or nil -- Cleanse Spirit or Purify Spirit
+			dispelTypes.Magic = _G.IsPlayerSpell(77130) or nil -- Purify Spirit
 		elseif playerClass == "DRUID" then
-			dispelTypes.Curse = IsPlayerSpell(2782) or IsPlayerSpell(88423) or nil -- Remove Corruption or Nature's Cure
-			dispelTypes.Magic = IsPlayerSpell(88423) or nil -- Nature's Cure
+			dispelTypes.Curse = _G.IsPlayerSpell(2782) or _G.IsPlayerSpell(88423) or nil -- Remove Corruption or Nature's Cure
+			dispelTypes.Magic = _G.IsPlayerSpell(88423) or nil -- Nature's Cure
 			dispelTypes.Poison = dispelTypes.Curse
 		elseif playerClass == "PRIEST"  then
-			dispelTypes.Disease = IsPlayerSpell(527) or nil -- Purify
-			dispelTypes.Magic = IsPlayerSpell(527) or IsPlayerSpell(32375) or nil -- Purify or Mass Dispel
+			dispelTypes.Disease = _G.IsPlayerSpell(527) or nil -- Purify
+			dispelTypes.Magic = _G.IsPlayerSpell(527) or _G.IsPlayerSpell(32375) or nil -- Purify or Mass Dispel
 		elseif playerClass == "MONK" then
-			dispelTypes.Disease = IsPlayerSpell(115450) or nil -- Detox
-			dispelTypes.Magic = IsPlayerSpell(115451) or nil -- Internal Medicine
+			dispelTypes.Disease = _G.IsPlayerSpell(115450) or nil -- Detox
+			dispelTypes.Magic = _G.IsPlayerSpell(115451) or nil -- Internal Medicine
 			dispelTypes.Poison = dispelTypes.Disease
 		elseif playerClass == "MAGE" then
-			dispelTypes.Curse = IsPlayerSpell(475) or nil -- Remove Curse
+			dispelTypes.Curse = _G.IsPlayerSpell(475) or nil -- Remove Curse
 		end
 	end
 end
