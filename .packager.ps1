487ee5983d8bfc9918da6e350686cb83a1bdb8f0
Val Voronov <i.lightspark@gmail.com>
2016-04-13 21:22:26 +0700

    Added my packager script.
    I'm too tired of accidentally deleting it all the time.

---
+++ .packager.ps1
@@ -0,0 +1,31 @@
+Set-Location $PSScriptRoot
+
+$curVersion = if ((Get-Content ".\oUF_LS.toc" | Where { $_ -match "Version: ([0-9]+\.[0-9]+)" } ) -match "([0-9]+\.[0-9]+)") {$matches[1]}
+$folderName = "oUF_LS"
+$zipName = $folderName + "-" + $curVersion + ".zip"
+
+$includedFiles = @(
+	".\init.lua",
+	".\oUF_LS.toc",
+	".\LICENSE.txt",
+	".\config\",
+	".\core\",
+	".\embeds\",
+	".\locales\",
+	".\media\",
+	".\modules\"
+)
+
+$filesToRemove = @(
+	".git",
+	"README.md"
+)
+
+Remove-Item * -Include @("*.zip", $folderName) -Recurse -Force
+
+New-Item -Name $folderName -ItemType Directory | Out-Null
+Copy-Item $includedFiles -Destination $folderName -Recurse
+Remove-Item $folderName -Include $filesToRemove -Recurse -Force
+Compress-Archive -Path $folderName -DestinationPath $zipName
+
+Remove-Item $folderName -Recurse -Force
