ba6854640f25f629db4e7b729fdea3b8b7df0fed
Val Voronov <i.lightspark@gmail.com>
2016-06-23 13:09:27 +0700

    Updated highlight element.

--- modules/unitframes/elements/highlight.lua
+++ modules/unitframes/elements/highlight.lua
@@ -6,7 +6,11 @@ local _G = _G
 
 local function Update(self, event)
 	local status = _G.UnitThreatSituation("player")
-	local color = status and M.colors.threat[status] or M.colors.class[E.PLAYER_CLASS]
+	local color = M.colors.class[E.PLAYER_CLASS]
+
+	if _G.UnitAffectingCombat("player") then
+		color = M.colors.threat[status and status + 1 or 1]
+	end
 
 	self.Spin1:SetVertexColor(color[1], color[2], color[3])
 	self.Spin2:SetVertexColor(color[1], color[2], color[3])
@@ -17,8 +21,8 @@ function UF:CreateStatusHighlight(parent)
 
 	local spin1 = parent:CreateTexture(nil, "BACKGROUND", nil, -1)
 	spin1:SetBlendMode("ADD")
-	spin1:SetTexture("Interface\\AddOns\\oUF_LS\\media\\minimap")
-	spin1:SetTexCoord(1 / 512, 169 / 512, 204 / 512, 372 / 512)
+	spin1:SetTexture("Interface\\AddOns\\oUF_LS\\media\\spinner")
+	spin1:SetTexCoord(1 / 256, 169 / 256, 1 / 256, 169 / 256)
 	spin1:SetVertexColor(color[1], color[2], color[3])
 	spin1:SetSize(168, 168)
 	spin1:SetPoint("CENTER", 0, 0)
@@ -26,9 +30,9 @@ function UF:CreateStatusHighlight(parent)
 
 	local spin2 = parent:CreateTexture(nil, "BACKGROUND", nil, -2)
 	spin2:SetBlendMode("ADD")
-	spin2:SetTexture("Interface\\AddOns\\oUF_LS\\media\\minimap")
+	spin2:SetTexture("Interface\\AddOns\\oUF_LS\\media\\spinner")
 	spin2:SetVertexColor(color[1], color[2], color[3])
-	spin2:SetTexCoord(169 / 512, 1 / 512, 204 / 512, 372 / 512)
+	spin2:SetTexCoord(169 / 256, 1 / 256, 169 / 256, 1 / 256)
 	spin2:SetSize(168, 168)
 	spin2:SetPoint("CENTER", 0, 0)
 	parent.Spin2 = spin2
