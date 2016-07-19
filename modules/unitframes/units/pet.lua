ff427de17973f29c781bbb2c90183ccf69fdd5ee
Val Voronov <i.lightspark@gmail.com>
2016-07-20 01:35:01 +0700

    Updated pet frame textures.

--- modules/unitframes/units/pet.lua
+++ modules/unitframes/units/pet.lua
@@ -11,24 +11,21 @@ function UF:ConstructPetFrame(frame)
 	frame:SetSize(42, 134)
 
 	local bg = frame:CreateTexture(nil, "BACKGROUND", nil, 2)
-	bg:SetAllPoints()
-	bg:SetTexture("Interface\\AddOns\\oUF_LS\\media\\frame_pet")
-	bg:SetTexCoord(84 / 256, 126 / 256, 0, 134 / 256)
+	bg:SetSize(38, 114)
+	bg:SetTexture("Interface\\AddOns\\oUF_LS\\media\\frame-pet-bg")
+	bg:SetTexCoord(1 / 64, 39 / 64, 1 / 128, 115 / 128)
+	bg:SetPoint("CENTER", 0, 0)
 
 	local cover = CreateFrame("Frame", "$parentCover", frame)
 	cover:SetFrameLevel(level + 3)
 	cover:SetAllPoints()
 	frame.Cover = cover
 
-	-- local tubes = cover:CreateTexture(nil, "ARTWORK", nil, 0)
-	-- tubes:SetAllPoints()
-	-- tubes:SetTexture("Interface\\AddOns\\oUF_LS\\media\\frame_pet")
-	-- tubes:SetTexCoord(0, 42 / 256, 0, 134 / 256)
-
 	local fg = cover:CreateTexture(nil, "ARTWORK", nil, 1)
-	fg:SetAllPoints()
-	fg:SetTexture("Interface\\AddOns\\oUF_LS\\media\\frame_pet")
-	fg:SetTexCoord(42 / 256, 84 / 256, 0, 134 / 256)
+	fg:SetSize(32, 70)
+	fg:SetTexture("Interface\\AddOns\\oUF_LS\\media\\frame-pet-fg")
+	fg:SetTexCoord(1 / 64, 33 / 64, 1 / 128, 71 / 128)
+	fg:SetPoint("CENTER", 0, 0)
 
 	local health = UF:CreateHealthBar(frame, 12, nil, true)
 	health:SetFrameLevel(level + 1)
@@ -56,12 +53,12 @@ function UF:ConstructPetFrame(frame)
 	powerText:SetJustifyH("RIGHT")
 	powerText:SetPoint("BOTTOMRIGHT", frame, "BOTTOMLEFT", 8, 14)
 
-	local debuffStatus = E:CreateFontString(cover, 12, "$parentDebuffStatus")
-	debuffStatus:SetWidth(14)
-	debuffStatus:SetWordWrap(true)
-	debuffStatus:SetDrawLayer("OVERLAY")
-	debuffStatus:SetPoint("CENTER")
-	frame:Tag(debuffStatus, "[ls:debuffstatus]")
+	local status = E:CreateFontString(cover, 12, "$parentDebuffStatus")
+	status:SetWidth(14)
+	status:SetWordWrap(true)
+	status:SetDrawLayer("OVERLAY")
+	status:SetPoint("CENTER")
+	frame:Tag(status, "[ls:debuffstatus]")
 
 	if C.units.pet.castbar then
 		frame.Castbar = UF:CreateCastBar(frame, 202, true, true)
@@ -70,9 +67,9 @@ function UF:ConstructPetFrame(frame)
 	end
 
 	local threat = UF:CreateThreat(frame)
-	threat:SetTexture("Interface\\AddOns\\oUF_LS\\media\\frame_pet")
-	threat:SetTexCoord(126 / 256, 168 / 256, 0 / 256, 100 / 256)
-	threat:SetSize(42, 100)
-	threat:SetPoint("CENTER", 0, 18)
+	threat:SetTexture("Interface\\AddOns\\oUF_LS\\media\\frame-pet-threat")
+	threat:SetTexCoord(1 / 64, 45 / 64, 1 / 64, 51 / 64)
+	threat:SetSize(44, 50)
+	threat:SetPoint("CENTER", 0, 0)
 	frame.Threat = threat
 end
