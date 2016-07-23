fd8bfc7529bff4942add766bbcbe98da8edc4e57
Val Voronov <i.lightspark@gmail.com>
2016-07-23 15:01:36 +0700

    Tweaked packager.

--- .packager.ps1
+++ .packager.ps1
@@ -1,12 +1,12 @@
 Set-Location $PSScriptRoot
 
-$curVersion = if ((Get-Content ".\oUF_LS.toc" | Where { $_ -match "Version: ([0-9]+\.[0-9]+)" } ) -match "([0-9]+\.[0-9]+)") {$matches[1]}
-$folderName = "oUF_LS"
+$curVersion = if ((Get-Content ".\ls_UI.toc" | Where { $_ -match "Version: ([0-9]+\.[0-9]+)" } ) -match "([0-9]+\.[0-9]+)") {$matches[1]}
+$folderName = "ls_UI"
 $zipName = $folderName + "-" + $curVersion + ".zip"
 
 $includedFiles = @(
 	".\init.lua",
-	".\oUF_LS.toc",
+	".\ls_UI.toc",
 	".\LICENSE.txt",
 	".\config\",
 	".\core\",
@@ -19,10 +19,8 @@ $includedFiles = @(
 $filesToRemove = @(
 	".git",
 	".packager.ps1",
-	"CHANGELOG",
-	"CHANGELOG.*",
-	"README",
-	"README.*"
+	"CHANGELOG*",
+	"README*"
 )
 
 Remove-Item * -Include @("*.zip", $folderName) -Recurse -Force
@@ -31,6 +29,6 @@ New-Item -Name $folderName -ItemType Directory | Out-Null
 Copy-Item $includedFiles -Destination $folderName -Recurse
 Remove-Item $folderName -Include $filesToRemove -Recurse -Force
 Compress-Archive -Path $folderName -DestinationPath $zipName
-Move-Item ".\oUF_LS-*.zip" -Destination "..\" -Force
+Move-Item ".\ls_UI-*.zip" -Destination "..\" -Force
 
 Remove-Item $folderName -Recurse -Force
