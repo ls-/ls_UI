da7dba6b6403c00eb95add833302a261469056b8
Val Voronov <i.lightspark@gmail.com>
2016-08-22 00:14:21 +0700

    Hacks...

---
+++ modules/blizzard/worldmap.lua
@@ -0,0 +1,33 @@
+local _, ns = ...
+local E, C, M, L = ns.E, ns.C, ns.M, ns.L
+local B = E:GetModule("Blizzard")
+
+-- Lua
+local _G = _G
+
+-- Mine
+function B:HandleWorldMap()
+	function _G.WorldMapFrame.UIElementsFrame.BountyBoard.GetDisplayLocation(self)
+		if _G.InCombatLockdown() then
+			return
+		end
+
+		return _G.WorldMapBountyBoardMixin.GetDisplayLocation(self)
+	end
+
+	function _G.WorldMapFrame.UIElementsFrame.ActionButton.GetDisplayLocation(self, useAlternateLocation)
+		if _G.InCombatLockdown() then
+			return
+		end
+
+		return _G.WorldMapActionButtonMixin.GetDisplayLocation(self, useAlternateLocation)
+	end
+
+	function _G.WorldMapFrame.UIElementsFrame.ActionButton.Refresh(self)
+		if _G.InCombatLockdown() then
+			return
+		end
+
+		_G.WorldMapActionButtonMixin.Refresh(self)
+	end
+end
