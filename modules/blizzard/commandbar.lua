651fa6556060be128af0c615fdb539b55a822c30
Val Voronov <i.lightspark@gmail.com>
2016-10-12 15:03:43 +0700

    Tweaked `OrderHallCommandBar` code a bit.

--- modules/blizzard/commandbar.lua
+++ modules/blizzard/commandbar.lua
@@ -1,5 +1,5 @@
 local _, ns = ...
-local E, C, M, L = ns.E, ns.C, ns.M, ns.L
+local E = ns.E
 local B = E:GetModule("Blizzard")
 
 -- Lua
@@ -9,51 +9,33 @@ local _G = _G
 local function CommandBar_OnEnter(self)
 	if not self.isShown then
 		self.isShown = true
-		self:SetPoint("TOP", 0, 0)
+		self:SetPoint("TOP", 0, 1)
 	end
 end
 
 local function CommandBar_OnLeave(self)
-	if not self:IsMouseOver(4, 0, -4, 4) then
+	if not self:IsMouseOver(0, -6, 0, 0) then
 		self.isShown = false
-		self:SetPoint("TOP", 0, 22)
-	end
-end
-
-local function SubFrame_OnEnter(self)
-	local bar = self:GetParent()
-
-	if not bar.isShown then
-		CommandBar_OnEnter(bar)
-	end
-end
-local function SubFrame_OnLeave(self)
-	local bar = self:GetParent()
-
-	if bar.isShown then
-		if not bar:IsMouseOver(4, 0, -4, 4) then
-			CommandBar_OnLeave(bar)
-		end
+		self:SetPoint("TOP", 0, 23)
 	end
 end
 
 function B:HandleCommandBar()
+	local isLoaded = true
+
 	if not _G.IsAddOnLoaded("Blizzard_OrderHallUI") then
-		E:ForceLoadAddOn("Blizzard_OrderHallUI")
+		isLoaded = _G.LoadAddOn("Blizzard_OrderHallUI")
 	end
 
-	local bar = _G.OrderHallCommandBar
+	if isLoaded then
+		_G.OrderHallCommandBar:ClearAllPoints()
+		_G.OrderHallCommandBar:SetPoint("TOP", 0, 23)
+		_G.OrderHallCommandBar:SetPoint("LEFT", 0, 0)
+		_G.OrderHallCommandBar:SetPoint("RIGHT", 0, 0)
+		_G.OrderHallCommandBar:SetHitRectInsets(0, 0, 0, -8)
+		_G.OrderHallCommandBar:SetScript("OnEnter", CommandBar_OnEnter)
+		_G.OrderHallCommandBar:SetScript("OnLeave", CommandBar_OnLeave)
 
-	bar:ClearAllPoints()
-	bar:SetPoint("TOP", 0, 22)
-	bar:SetPoint("LEFT", 0, 0)
-	bar:SetPoint("RIGHT", 0, 0)
-	bar:SetScript("OnEnter", CommandBar_OnEnter)
-	bar:SetScript("OnLeave", CommandBar_OnLeave)
-
-	bar.CurrencyHitTest:HookScript("OnEnter", SubFrame_OnEnter)
-	bar.CurrencyHitTest:HookScript("OnLeave", SubFrame_OnLeave)
-
-	bar.WorldMapButton:HookScript("OnEnter", SubFrame_OnEnter)
-	bar.WorldMapButton:HookScript("OnLeave", SubFrame_OnLeave)
+		E:ForceHide(_G.OrderHallCommandBar.WorldMapButton)
+	end
 end
