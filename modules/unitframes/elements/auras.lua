147d4f81acbe7159b74f5408c18fd582b9e26c5f
Val Voronov <i.lightspark@gmail.com>
2017-03-24 16:16:35 +0700

    Blacklisted useless auras.

--- modules/unitframes/elements/auras.lua
+++ modules/unitframes/elements/auras.lua
@@ -27,6 +27,62 @@ for _, id in next, C_MountJournal.GetMountIDs() do
 	MOUNTS[spellID] = true
 end
 
+local BLACKLIST = {
+	[  8326] = true, -- Ghost
+	[ 26013] = true, -- Deserter
+	[ 39953] = true, -- A'dal's Song of Battle
+	[ 57819] = true, -- Argent Champion
+	[ 57820] = true, -- Ebon Champion
+	[ 57821] = true, -- Champion of the Kirin Tor
+	[ 71041] = true, -- Dungeon Deserter
+	[ 72968] = true, -- Precious's Ribbon
+	[ 85612] = true, -- Fiona's Lucky Charm
+	[ 85613] = true, -- Gidwin's Weapon Oil
+	[ 85614] = true, -- Tarenar's Talisman
+	[ 85615] = true, -- Pamela's Doll
+	[ 85616] = true, -- Vex'tul's Armbands
+	[ 85617] = true, -- Argus' Journal
+	[ 85618] = true, -- Rimblat's Stone
+	[ 85619] = true, -- Beezil's Cog
+	[ 93337] = true, -- Champion of Ramkahen
+	[ 93339] = true, -- Champion of the Earthen Ring
+	[ 93341] = true, -- Champion of the Guardians of Hyjal
+	[ 93347] = true, -- Champion of Therazane
+	[ 93368] = true, -- Champion of the Wildhammer Clan
+	[ 93795] = true, -- Stormwind Champion
+	[ 93805] = true, -- Ironforge Champion
+	[ 93806] = true, -- Darnassus Champion
+	[ 93811] = true, -- Exodar Champion
+	[ 93816] = true, -- Gilneas Champion
+	[ 93821] = true, -- Gnomeregan Champion
+	[ 93825] = true, -- Orgrimmar Champion
+	[ 93827] = true, -- Darkspear Champion
+	[ 93828] = true, -- Silvermoon Champion
+	[ 93830] = true, -- Bilgewater Champion
+	[ 94158] = true, -- Champion of the Dragonmaw Clan
+	[ 94462] = true, -- Undercity Champion
+	[ 94463] = true, -- Thunder Bluff Champion
+	[ 97340] = true, -- Guild Champion
+	[ 97341] = true, -- Guild Champion
+	[126434] = true, -- Tushui Champion
+	[126436] = true, -- Huojin Champion
+	[143625] = true, -- Brawling Champion
+	[170616] = true, -- Pet Deserter
+	[182957] = true, -- Treasures of Stormheim
+	[182958] = true, -- Treasures of Azsuna
+	[185719] = true, -- Treasures of Val'sharah
+	[186401] = true, -- Sign of the Skirmisher
+	[186403] = true, -- Sign of Battle
+	[186404] = true, -- Sign of the Emissary
+	[186406] = true, -- Sign of the Critter
+	[188741] = true, -- Treasures of Highmountain
+	[199416] = true, -- Treasures of Suramar
+	[225787] = true, -- Sign of the Warrior
+	[225788] = true, -- Sign of the Emissary
+	[227723] = true, -- Mana Divining Stone
+	[231115] = true, -- Treasures of Broken Shore
+}
+
 local function SetVertexColorOverride(self, r, g, b)
 	local button = self:GetParent()
 
@@ -101,6 +157,11 @@ end
 
 local filterFunctions = {
 	default = function(frame, unit, aura, _, _, _, count, debuffType, duration, _, caster, isStealable, _, spellID, _, isBossAura)
+		-- blacklist
+		if BLACKLIST[spellID] then
+			return false
+		end
+
 		local config = frame.cfg
 		local isFriend = UnitIsFriend("player", unit)
 		local friendlyBuffFlag = (isFriend and not aura.isDebuff) and E:GetPlayerSpecFlag() or 0x00000000
