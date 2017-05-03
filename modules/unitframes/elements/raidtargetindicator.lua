525fddf34b23a8d2a51b2a849875347cd4ad318a
Val Voronov <i.lightspark@gmail.com>
2017-05-03 13:55:42 +0700

    Added raid target widget.

---
+++ modules/unitframes/elements/raidtargetindicator.lua
@@ -0,0 +1,34 @@
+local _, ns = ...
+local E, C, M, L, P = ns.E, ns.C, ns.M, ns.L, ns.P
+local UF = P:GetModule("UnitFrames")
+
+-- Lua
+local _G = getfenv(0)
+
+-- Mine
+function UF:CreateRaidTargetIndicator(parent)
+	local element = parent:CreateTexture("$parentRaidIcon", "ARTWORK", nil, 3)
+
+	return element
+end
+
+function UF:UpdateRaidTargetIndicator(frame)
+	local config = frame._config.raid_target
+	local element = frame.RaidTargetIndicator
+
+	element:SetSize(config.size, config.size)
+
+	local point1 = config.point1
+
+	element:SetPoint(point1.p, E:ResolveAnchorPoint(frame, point1.anchor), point1.rP, point1.x, point1.y)
+
+	if config.enabled and not frame:IsElementEnabled("RaidTargetIndicator") then
+		frame:EnableElement("RaidTargetIndicator")
+	elseif not config.enabled and frame:IsElementEnabled("RaidTargetIndicator") then
+		frame:DisableElement("RaidTargetIndicator")
+	end
+
+	if frame:IsElementEnabled("RaidTargetIndicator") then
+		element:ForceUpdate()
+	end
+end
