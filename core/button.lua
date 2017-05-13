22a9aeddfd393842e874812a40c32258401c89d7
Val Voronov <i.lightspark@gmail.com>
2017-05-13 23:23:21 +0700

    Cleanup.

--- core/button.lua
+++ core/button.lua
@@ -3,7 +3,7 @@ ActionButtonTemplate Draw Layers:
 BACKGROUND:
 - Icon -- 0
 BORDER:
-
+-
 ARTWORK:
 - NormalTexture -- 0
 - PushedTexture -- 0
@@ -25,9 +25,12 @@ local E, C, M, L, P = ns.E, ns.C, ns.M, ns.L, ns.P
 
 -- Lua
 local _G = getfenv(0)
-local string = _G.string
 local hooksecurefunc = _G.hooksecurefunc
 local next = _G.next
+local s_gsub = _G.string.gsub
+local s_match = _G.string.match
+local s_format = _G.string.format
+local s_utf8sub = _G.string.utf8sub
 
 -- Blizz
 local ATTACK_BUTTON_FLASH_TIME = _G.ATTACK_BUTTON_FLASH_TIME
@@ -74,13 +77,13 @@ end
 local function SetTextHook(self, text)
 	if not text then return end
 
-	self:SetFormattedText("%s", string.gsub(text, "[ .()]", ""))
+	self:SetFormattedText("%s", s_gsub(text, "[ .()]", ""))
 end
 
 local function SetFormattedTextHook(self, pattern, text)
 	if not pattern then return end
 
-	self:SetText(string.format(string.gsub(pattern, "[ .()]", ""), text))
+	self:SetText(s_format(s_gsub(pattern, "[ .()]", ""), text))
 end
 
 local function SetHotKeyTextHook(self)
@@ -89,8 +92,8 @@ local function SetHotKeyTextHook(self)
 	local bType = button.buttonType
 
 	if not bType then
-		if name and not string.match(name, "Stance") then
-			if string.match(name, "PetAction") then
+		if name and not s_match(name, "Stance") then
+			if s_match(name, "PetAction") then
 				bType = "BONUSACTIONBUTTON"
 			else
 				bType = "ACTIONBUTTON"
@@ -103,28 +106,28 @@ local function SetHotKeyTextHook(self)
 	local text = bType and _G.GetBindingKey(bType..button:GetID()) or ""
 
 	if text and text ~= "" then
-		text = string.gsub(text, "SHIFT%-", "S")
-		text = string.gsub(text, "CTRL%-", "C")
-		text = string.gsub(text, "ALT%-", "A")
-		text = string.gsub(text, "BUTTON1", "LM")
-		text = string.gsub(text, "BUTTON2", "RM")
-		text = string.gsub(text, "BUTTON3", "MM")
-		text = string.gsub(text, "BUTTON", "M")
-		text = string.gsub(text, "MOUSEWHEELDOWN", "WD")
-		text = string.gsub(text, "MOUSEWHEELUP", "WU")
-		text = string.gsub(text, "NUMPADDECIMAL", "N.")
-		text = string.gsub(text, "NUMPADDIVIDE", "N/")
-		text = string.gsub(text, "NUMPADMINUS", "N-")
-		text = string.gsub(text, "NUMPADMULTIPLY", "N*")
-		text = string.gsub(text, "NUMPADPLUS", "N+")
-		text = string.gsub(text, "NUMPAD", "N")
-		text = string.gsub(text, "PAGEDOWN", "PD")
-		text = string.gsub(text, "PAGEUP", "PU")
-		text = string.gsub(text, "SPACE", "Sp")
-		text = string.gsub(text, "DOWN", "Dn")
-		text = string.gsub(text, "LEFT", "Lt")
-		text = string.gsub(text, "RIGHT", "Rt")
-		text = string.gsub(text, "UP", "Up")
+		text = s_gsub(text, "SHIFT%-", "S")
+		text = s_gsub(text, "CTRL%-", "C")
+		text = s_gsub(text, "ALT%-", "A")
+		text = s_gsub(text, "BUTTON1", "LM")
+		text = s_gsub(text, "BUTTON2", "RM")
+		text = s_gsub(text, "BUTTON3", "MM")
+		text = s_gsub(text, "BUTTON", "M")
+		text = s_gsub(text, "MOUSEWHEELDOWN", "WD")
+		text = s_gsub(text, "MOUSEWHEELUP", "WU")
+		text = s_gsub(text, "NUMPADDECIMAL", "N.")
+		text = s_gsub(text, "NUMPADDIVIDE", "N/")
+		text = s_gsub(text, "NUMPADMINUS", "N-")
+		text = s_gsub(text, "NUMPADMULTIPLY", "N*")
+		text = s_gsub(text, "NUMPADPLUS", "N+")
+		text = s_gsub(text, "NUMPAD", "N")
+		text = s_gsub(text, "PAGEDOWN", "PD")
+		text = s_gsub(text, "PAGEUP", "PU")
+		text = s_gsub(text, "SPACE", "Sp")
+		text = s_gsub(text, "DOWN", "Dn")
+		text = s_gsub(text, "LEFT", "Lt")
+		text = s_gsub(text, "RIGHT", "Rt")
+		text = s_gsub(text, "UP", "Up")
 	end
 
 	self:SetFormattedText("%s", text or "")
@@ -138,7 +141,7 @@ local function SetMacroTextHook(self, text)
 		text = text or bName:GetText()
 
 		if text then
-			bName:SetFormattedText("%s", string.utf8sub(text, 1, 4))
+			bName:SetFormattedText("%s", s_utf8sub(text, 1, 4))
 		end
 	end
 end
@@ -334,7 +337,7 @@ function E:SkinAuraButton(button)
 	if bBorder then
 		bBorder:SetTexture(nil)
 
-		if string.gsub(name, "%d", "") == "TempEnchant" then
+		if s_gsub(name, "%d", "") == "TempEnchant" then
 			button:SetBorderColor(M.COLORS.PURPLE:GetRGB())
 		else
 			hooksecurefunc(bBorder, "SetVertexColor", SetVertexColorHook)
@@ -577,7 +580,7 @@ function E:CreateButton(parent, name, isSandwich, isSecure)
 	SetHighlightTexture(button)
 
 	if isSandwich then
-		local cover = _G.CreateFrame("Frame", "$parentCover", button)
+		local cover = _G.CreateFrame("Frame", nil, button)
 		cover:SetFrameLevel(button:GetFrameLevel() + 2)
 		cover:SetAllPoints()
 		button.Cover = cover
@@ -608,9 +611,10 @@ function E:CreateCheckButton(parent, name, isSandwich, isSecure)
 	SetCheckedTexture(button)
 
 	if isSandwich then
-		local cover = _G.CreateFrame("Frame", "$parentCover", button)
+		local cover = _G.CreateFrame("Frame", nil, button)
 		cover:SetFrameLevel(button:GetFrameLevel() + 2)
 		cover:SetAllPoints()
+		button.Cover = cover
 
 		count:SetParent(cover)
 	end
