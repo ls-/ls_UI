bb2d5abd99861daff315ff341ec84c0182f5dfe2
Val Voronov <i.lightspark@gmail.com>
2018-01-26 10:16:59 +0700

    Updated pet battle UI
    - Added fading support;
    - Added movers for pet selector and turn timer.

--- modules/bars/petbattle.lua
+++ modules/bars/petbattle.lua
@@ -7,16 +7,11 @@ local _G = getfenv(0)
 local hooksecurefunc = _G.hooksecurefunc
 local next = _G.next
 
--- Blizz
-local CreateFrame = _G.CreateFrame
-local FlowContainer_PauseUpdates = _G.FlowContainer_PauseUpdates
-local RegisterStateDriver = _G.RegisterStateDriver
-
 -- Mine
 local isInit = false
 
--- DO NOT add it to D table
 local CFG = {
+	visible = true,
 	num = 6,
 	size = 32,
 	spacing = 4,
@@ -31,13 +26,30 @@ local CFG = {
 		x = 0,
 		y = 16
 	},
+	fade = {
+		enabled = false,
+	},
 }
 
 function MODULE.CreatePetBattleBar()
 	if not isInit then
 		local config = MODULE:IsRestricted() and CFG or C.db.profile.bars.pet_battle
 
-		local bar = CreateFrame("Frame", "LSPetBattleBar", UIParent, "SecureHandlerBaseTemplate")
+		local bar = CreateFrame("Frame", "LSPetBattleBar", UIParent, "SecureHandlerStateTemplate")
+
+		MODULE:AddBar("pet_battle", bar)
+
+		-- hacks
+		bar.Update = function(self)
+			self._config = MODULE:IsRestricted() and CFG or C.db.profile.bars.pet_battle
+
+			MODULE:UpdateBarFading(self)
+			MODULE:UpdateBarVisibility(self)
+
+			if self._buttons then
+				E:UpdateBarLayout(self)
+			end
+		end
 
 		if MODULE:IsRestricted() then
 			MODULE:ActionBarController_AddWidget(bar, "PET_BATTLE_BAR")
@@ -46,19 +58,6 @@ function MODULE.CreatePetBattleBar()
 
 			bar:SetPoint(point.p, point.anchor, point.rP, point.x, point.y)
 			E:CreateMover(bar)
-		end
-
-		RegisterStateDriver(bar, "visibility", config.visibility)
-
-		MODULE:AddBar("pet_battle", bar)
-
-		-- hacks
-		bar.Update = function(self)
-			if self._buttons then
-				self._config = MODULE:IsRestricted() and CFG or C.db.profile.bars.pet_battle
-
-				E:UpdateBarLayout(self)
-			end
 		end
 
 		hooksecurefunc("PetBattleFrame_UpdateActionBarLayout", function()
@@ -71,8 +70,9 @@ function MODULE.CreatePetBattleBar()
 				PetBattleFrame.BottomFrame.ForfeitButton
 			}
 
-			for _, button in next, buttons do
+			for id, button in next, buttons do
 				button._parent = bar
+				button._command = "ACTIONBUTTON"..id
 				button:SetParent(bar)
 
 				E:SkinPetBattleButton(button)
@@ -82,6 +82,8 @@ function MODULE.CreatePetBattleBar()
 
 			bar:Update()
 		end)
+
+		bar:Update()
 
 		-- Cleanup
 		FlowContainer_PauseUpdates(PetBattleFrame.BottomFrame.FlowFrame)
@@ -96,12 +98,24 @@ function MODULE.CreatePetBattleBar()
 		E:ForceHide(PetBattleFrame.BottomFrame.TurnTimer.ArtFrame2)
 		E:ForceHide(PetBattleFrame.BottomFrame, true, true)
 
-		PetBattleFrame.BottomFrame.TurnTimer:SetParent(bar)
-		PetBattleFrame.BottomFrame.TurnTimer:ClearAllPoints()
-		PetBattleFrame.BottomFrame.TurnTimer:SetPoint("BOTTOM", "UIParent", "BOTTOM", 0, 60)
+		local timer = CreateFrame("Frame", "LSPetBattleTurnTimer", UIParent, "SecureHandlerStateTemplate")
+		timer:SetSize(474, 28)
+		timer:SetPoint("BOTTOM", "UIParent", "BOTTOM", 0, 60)
+		E:CreateMover(timer)
+		RegisterStateDriver(timer, "visibility", "[petbattle] show; hide")
 
-		PetBattleFrame.BottomFrame.PetSelectionFrame:SetParent(bar)
+		PetBattleFrame.BottomFrame.TurnTimer:SetParent(timer)
+		PetBattleFrame.BottomFrame.TurnTimer:ClearAllPoints()
+		PetBattleFrame.BottomFrame.TurnTimer:SetPoint("TOPLEFT", timer, "TOPLEFT", 1, -1)
+
+		local selector = CreateFrame("Frame", "LSPetBattlePetSelector", UIParent, "SecureHandlerStateTemplate")
+		selector:SetSize(636, 200)
+		selector:SetPoint("TOP", "UIParent", "TOP", 0, -194)
+		E:CreateMover(selector)
+		RegisterStateDriver(selector, "visibility", "[petbattle] show; hide")
+
+		PetBattleFrame.BottomFrame.PetSelectionFrame:SetParent(selector)
 		PetBattleFrame.BottomFrame.PetSelectionFrame:ClearAllPoints()
-		PetBattleFrame.BottomFrame.PetSelectionFrame:SetPoint("BOTTOM", "UIParent", "BOTTOM", 0, 92)
+		PetBattleFrame.BottomFrame.PetSelectionFrame:SetPoint("BOTTOM", selector, "BOTTOM", 0, 0)
 	end
 end
