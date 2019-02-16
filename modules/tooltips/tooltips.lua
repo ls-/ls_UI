8860028812723beedc7f791e329c69b5cd4aeff1
Val Voronov <i.lightspark@gmail.com>
2019-02-16 18:28:28 +0700

    :lipstick:

--- modules/tooltips/tooltips.lua
+++ modules/tooltips/tooltips.lua
@@ -288,13 +288,10 @@ local function addInspectInfo(tooltip, unit, classColorHEX, numTries)
 
 		tooltip:AddLine(SPECIALIZATION:format(classColorHEX, inspectGUIDCache[unitGUID].specName), 1, 1, 1)
 		tooltip:AddLine(ITEM_LEVEL:format(inspectGUIDCache[unitGUID].itemLevel), 1, 1, 1)
-	else
-		if lastGUID ~= unitGUID then
-			NotifyInspect(unit)
-
-			lastGUID = unitGUID
-		end
-
+	elseif lastGUID ~= unitGUID then
+		lastGUID = unitGUID
+		
+		NotifyInspect(unit)
 		E:RegisterEvent("INSPECT_READY", INSPECT_READY)
 	end
 end
