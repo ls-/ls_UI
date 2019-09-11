49e940d499a5e6a252d88c187288583f5d3d5208
ls- <i.lightspark@gmail.com>
2019-09-11 19:34:32 +0700

    💄

--- config/core.lua
+++ config/core.lua
@@ -701,9 +701,8 @@ do
 	local badTags = {}
 
 	local function getTagName(tag)
-		local tagStart = (tag:match('>+()') or 2)
-		local tagEnd = tag:match('.*()<+')
-		tagEnd = (tagEnd and tagEnd - 1) or -2
+		local tagStart = tag:match('>+()') or 2
+		local tagEnd = (tag:match('.-()<') or -1) - 1
 
 		return tag:sub(tagStart, tagEnd), tagStart, tagEnd
 	end
