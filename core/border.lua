6190d7c11103cf70f47fad4048a8439afd8b5c0b
Val Voronov <i.lightspark@gmail.com>
2021-06-21 22:22:11 +0700

    Let's goooooo...
    ... fml, I'll do the rest tomorrow 💤

--- core/border.lua
+++ core/border.lua
@@ -3,6 +3,8 @@ local E, C, M, L, P = ns.E, ns.C, ns.M, ns.L, ns.P
 
 -- Lua
 local _G = getfenv(0)
+local collectgarbage = _G.collectgarbage
+local debugprofilestop = _G.debugprofilestop
 local next = _G.next
 local type = _G.type
 local unpack = _G.unpack
@@ -12,6 +14,11 @@ local sections = {"TOPLEFT", "TOPRIGHT", "BOTTOMLEFT", "BOTTOMRIGHT", "TOP", "BO
 local objectToWidget = {}
 
 local function onSizeChanged(self, w, h)
+	local timeStart, memStart = 0, 0
+	if P.PROFILING then
+		timeStart, memStart = debugprofilestop(), collectgarbage("count")
+	end
+
 	local border = objectToWidget[self]
 
 	local tile = (w + 2 * border.__offset) / 16
@@ -21,19 +28,37 @@ local function onSizeChanged(self, w, h)
 	tile = (h + 2 * border.__offset) / 16
 	border.LEFT:SetTexCoord(0, 0.125, 0, tile)
 	border.RIGHT:SetTexCoord(0.125, 0.25, 0, tile)
+
+	if P.PROFILING then
+		P:LogDebugInfo(self:GetDebugName() .. ":OnSizeChanged", debugprofilestop() - timeStart, collectgarbage("count") - memStart)
+	end
 end
 
 local border_proto = {}
 
 function border_proto:SetOffset(offset)
+	local timeStart, memStart = 0, 0
+	if P.PROFILING then
+		timeStart, memStart = debugprofilestop(), collectgarbage("count")
+	end
+
 	self.__offset = offset
 	self.TOPLEFT:SetPoint("BOTTOMRIGHT", self.__parent, "TOPLEFT", -offset, offset)
 	self.TOPRIGHT:SetPoint("BOTTOMLEFT", self.__parent, "TOPRIGHT", offset, offset)
 	self.BOTTOMLEFT:SetPoint("TOPRIGHT", self.__parent, "BOTTOMLEFT", -offset, -offset)
 	self.BOTTOMRIGHT:SetPoint("TOPLEFT", self.__parent, "BOTTOMRIGHT", offset, -offset)
+
+	if P.PROFILING then
+		P:LogDebugInfo(self:GetDebugName() .. ":SetOffset", debugprofilestop() - timeStart, collectgarbage("count") - memStart)
+	end
 end
 
 function border_proto:SetTexture(texture)
+	local timeStart, memStart = 0, 0
+	if P.PROFILING then
+		timeStart, memStart = debugprofilestop(), collectgarbage("count")
+	end
+
 	if type(texture) == "table" then
 		for _, v in next, sections do
 			self[v]:SetColorTexture(unpack(texture))
@@ -47,9 +72,18 @@ function border_proto:SetTexture(texture)
 			end
 		end
 	end
+
+	if P.PROFILING then
+		P:LogDebugInfo(self:GetDebugName() .. ":SetTexture", debugprofilestop() - timeStart, collectgarbage("count") - memStart)
+	end
 end
 
 function border_proto:SetSize(size)
+	local timeStart, memStart = 0, 0
+	if P.PROFILING then
+		timeStart, memStart = debugprofilestop(), collectgarbage("count")
+	end
+
 	self.__size = size
 	self.TOPLEFT:SetSize(size, size)
 	self.TOPRIGHT:SetSize(size, size)
@@ -61,23 +95,54 @@ function border_proto:SetSize(size)
 	self.RIGHT:SetWidth(size)
 
 	onSizeChanged(self.__parent, self.__parent:GetWidth(), self.__parent:GetHeight())
+
+	if P.PROFILING then
+		P:LogDebugInfo(self:GetDebugName() .. ":SetSize", debugprofilestop() - timeStart, collectgarbage("count") - memStart)
+	end
 end
 
 function border_proto:Hide()
+	local timeStart, memStart = 0, 0
+	if P.PROFILING then
+		timeStart, memStart = debugprofilestop(), collectgarbage("count")
+	end
+
 	for _, v in next, sections do
 		self[v]:Hide()
+	end
+
+	if P.PROFILING then
+		P:LogDebugInfo(self:GetDebugName() .. ":Hide", debugprofilestop() - timeStart, collectgarbage("count") - memStart)
 	end
 end
 
 function border_proto:Show()
+	local timeStart, memStart = 0, 0
+	if P.PROFILING then
+		timeStart, memStart = debugprofilestop(), collectgarbage("count")
+	end
+
 	for _, v in next, sections do
 		self[v]:Show()
+	end
+
+	if P.PROFILING then
+		P:LogDebugInfo(self:GetDebugName() .. ":Show", debugprofilestop() - timeStart, collectgarbage("count") - memStart)
 	end
 end
 
 function border_proto:SetShown(isShown)
+	local timeStart, memStart = 0, 0
+	if P.PROFILING then
+		timeStart, memStart = debugprofilestop(), collectgarbage("count")
+	end
+
 	for _, v in next, sections do
 		self[v]:SetShown(isShown)
+	end
+
+	if P.PROFILING then
+		P:LogDebugInfo(self:GetDebugName() .. ":SetShown", debugprofilestop() - timeStart, collectgarbage("count") - memStart)
 	end
 end
 
@@ -86,14 +151,32 @@ function border_proto:GetVertexColor()
 end
 
 function border_proto:SetVertexColor(r, g, b, a)
+	local timeStart, memStart = 0, 0
+	if P.PROFILING then
+		timeStart, memStart = debugprofilestop(), collectgarbage("count")
+	end
+
 	for _, v in next, sections do
 		self[v]:SetVertexColor(r, g, b, a)
+	end
+
+	if P.PROFILING then
+		P:LogDebugInfo(self:GetDebugName() .. ":SetVertexColor", debugprofilestop() - timeStart, collectgarbage("count") - memStart)
 	end
 end
 
 function border_proto:SetAlpha(a)
+	local timeStart, memStart = 0, 0
+	if P.PROFILING then
+		timeStart, memStart = debugprofilestop(), collectgarbage("count")
+	end
+
 	for _, v in next, sections do
 		self[v]:SetAlpha(a)
+	end
+
+	if P.PROFILING then
+		P:LogDebugInfo(self:GetDebugName() .. ":SetAlpha", debugprofilestop() - timeStart, collectgarbage("count") - memStart)
 	end
 end
 
@@ -101,7 +184,16 @@ function border_proto:IsObjectType(t)
 	return t == "Border"
 end
 
+function border_proto:GetDebugName()
+	return self.__parent:GetDebugName() .. ".Border"
+end
+
 function E:CreateBorder(parent, drawLayer, drawSubLevel)
+	local timeStart, memStart = 0, 0
+	if P.PROFILING then
+		timeStart, memStart = debugprofilestop(), collectgarbage("count")
+	end
+
 	local border = P:Mixin({__parent = parent}, border_proto)
 
 	for _, v in next, sections do
@@ -132,6 +224,10 @@ function E:CreateBorder(parent, drawLayer, drawSubLevel)
 
 	border:SetOffset(-8)
 	border:SetSize(16)
+
+	if P.PROFILING then
+		P:LogDebugInfo("E:CreateBorder", debugprofilestop() - timeStart, collectgarbage("count") - memStart)
+	end
 
 	return border
 end
