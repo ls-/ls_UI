270e2e5838495cc98a9a8a5e2d101c14f8ea7898
Val Voronov <i.lightspark@gmail.com>
2023-03-04 09:30:12 +0700

    📜

--- ls_UI/core/changelog.lua
+++ ls_UI/core/changelog.lua
@@ -6,12 +6,5 @@ local _G = getfenv(0)
 
 -- Mine
 E.CHANGELOG = [[
-### Unit Frames
-
-- Added the "Health" option to fading. It's controlled by the player's health, and if it's below
-  <100% the frame will fade in.
-- Added an option to disable status icons. These are the round role, class, etc icons at the bottom
-  of a frame.
-- Removed fading options from pet, target of focus, and target of target frames. These are now
-  controlled by the fading options of player, focus, and target frames respectively.
+- Fixed fading. Adding new stuff is never easy...
 ]]
