97a4f3a6a52e4d10a320f0fba152461c51e81b20
Val Voronov <i.lightspark@gmail.com>
2025-12-01 23:03:04 +0700

    📜

--- ls_UI/core/changelog.lua
+++ ls_UI/core/changelog.lua
@@ -6,5 +6,7 @@ local _G = getfenv(0)
 
 -- Mine
 E.CHANGELOG = [[
-- Added 11.2.5 support.
+- Added 11.2.7 support.
+
+NOTE: Unfortunately, the addon will be retired come Midnight. Parts of it like the minimap, xp bar, and Blizz tweaks will be turned into separate addons, but the rest (the action bars, unit frames, etc) will be scrapped.
 ]]
